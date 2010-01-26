e5f07cdc8f763adee82c31950500a5ebd20158fd
Thomas Graff <zograff@hotmail.com>
2010-01-26 06:51:26 +0100

    Update run to call main ui class.

--- run.py
+++ run.py
@@ -5,17 +5,12 @@ from enthought.pyface.api import GUI
 
 # Local imports
 from dataset_collection import DatasetCollection
-from datasets_ui import DsViewHandler
+from main_ui import MainUi
 
-
-def main():
-    """Run the application. """
-    dc = DatasetCollection()
-    ui = DsViewHandler().edit_traits( context = dc )
-    GUI().start_event_loop()
 
 
 if __name__ == '__main__':
-    main()
+    mother = MainUi()
+    mother.edit_traits()
 
 #### EOF ######################################################################
