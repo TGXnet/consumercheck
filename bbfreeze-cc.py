1778eb5fa0dabe60da3d0b422282670f175b02fa
Thomas Graff <graff.thomas@gmail.com>
2011-06-14 21:47:46 +0200

    Update win dist build files

--- bbfreeze-cc.py
+++ bbfreeze-cc.py
@@ -1,10 +1,14 @@
-
 from bbfreeze import Freezer
 
-frezer = Freezer(
-    "dist_bbfreeze",
-    includes=("_strptime",),
-    excludes=(),
-    )
-frezer.addScript("run.py")
-frezer()    # starts the freezing process
+includes = ('')
+exclude3 = ('pydoc', 'pydoc_topics', 'pkg_resources', 'doctest', 'difflib',
+            'cookielib', 'urllib', 'urllib2' 'acb', 'anydbm', 'ast', 'base64',
+            'pstats', 'TiffImagePlugin', 'uuid', 'modulefinder', 'xml', 'PIL')
+excludes = ('pydoc', 'pydoc_topics', 'doctest',
+            'TiffImagePlugin', 'modulefinder', 'xml', 'PIL')
+
+freeze = Freezer("consumercheck-0.4.1", includes=includes, excludes=excludes)
+freeze.addScript("consumercheck.py", gui_only=True)
+freeze.use_compression = 0
+freeze.include_py = True
+freeze()    # starts the freezing process
--- make_bbfreeze.bat
+++ make_bbfreeze.bat
@@ -1,13 +1,29 @@
-rem *** Used to create a Python exe 
+rem *** Used to create a Python exe
 
 rem ***** get rid of all the old files in the build folder
-rd /S /Q dist_bbfreeze
+rd /S /Q consumercheck-0.4.1
+rem pause "removed"
 
 rem ***** create the exe
-python bbfreeze-cc.py >make_bbfreeze.log
+python bbfreeze-cc.py>bbfreeze-cc.log
+rem pause "freezed"
 
-xcopy R-2.11.1 dist_bbfreeze\R-2.11.1\ /S /Q
-xcopy testdata dist_bbfreeze\testdata\ /S /Q
+rem *** copy files
+xcopy R-2.11.1 consumercheck-0.4.1\R-2.11.1\ /S /Q
+xcopy datasets consumercheck-0.4.1\datasets\ /S /Q
+
+rem *** Remove not used libaries
+del /Q consumercheck-0.4.1\QtWebKit4.dll
+del /Q consumercheck-0.4.1\QtGui4.dll
+del /Q consumercheck-0.4.1\PyQt4.QtGui.pyd
+del /Q consumercheck-0.4.1\QtCore4.dll
+del /Q consumercheck-0.4.1\PyQt4.QtCore.pyd
+del /Q consumercheck-0.4.1\QtNetwork4.dll
+del /Q consumercheck-0.4.1\_ssl.pyd
+del /Q consumercheck-0.4.1\PIL._imaging.pyd
+del /Q consumercheck-0.4.1\PyQt4.QtWebKit.pyd
+
+rem rd /S /Q consumercheck-0.4.1\traitsbackendqt-3.5.0-py2.6.egg
 
 rem **** pause so we can see the exit codes
 pause "done...hit a key to exit"
