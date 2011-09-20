fa352081d7a7d2b7fd48cf0df4493eda233d6b3f
Thomas Graff <graff.thomas@gmail.com>
2011-09-20 08:47:46 +0200

    Refactored build script

--- make_bbfreeze.bat
+++ make_bbfreeze.bat
@@ -1,25 +1,7 @@
 rem *** Used to create a Python exe
 
-rem ***** get rid of all the old files in the build folder
-rem rd /S /Q consumercheck-0.5.0
-rem pause "removed"
-
 rem ***** create the exe
 python bbfreeze-cc.py>bbfreeze-cc.log
-rem pause "freezed"
-
-rem *** Remove not used libaries
-rem del /Q consumercheck-0.5.0\QtWebKit4.dll
-rem del /Q consumercheck-0.5.0\QtGui4.dll
-rem del /Q consumercheck-0.5.0\PyQt4.QtGui.pyd
-rem del /Q consumercheck-0.5.0\QtCore4.dll
-rem del /Q consumercheck-0.5.0\PyQt4.QtCore.pyd
-rem del /Q consumercheck-0.5.0\QtNetwork4.dll
-rem del /Q consumercheck-0.5.0\_ssl.pyd
-rem del /Q consumercheck-0.5.0\PIL._imaging.pyd
-rem del /Q consumercheck-0.5.0\PyQt4.QtWebKit.pyd
-
-rem rd /S /Q consumercheck-0.5.0\traitsbackendqt-3.5.0-py2.6.egg
 
 rem **** pause so we can see the exit codes
 pause "done...hit a key to exit"
