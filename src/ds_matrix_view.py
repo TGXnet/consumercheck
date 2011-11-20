5e38b656ca647f9ee9f18607d4b16cb72167d4e2
Thomas Graff <graff.thomas@gmail.com>
2011-11-20 14:17:42 +0100

    Adde matrix view window
    See ticket:10

--- src/ds_matrix_view.py
+++ src/ds_matrix_view.py
@@ -4,93 +4,76 @@ import tempfile
 from numpy import savetxt
 
 # Enthough imports
-from traits.api import Instance, Button, Property
-from traitsui.api import Controller, View, Item, TabularEditor
+from traits.api import Button, List, Property
+from traitsui.api import Handler, View, Item, TabularEditor
 from traitsui.tabular_adapter import TabularAdapter
 from traitsui.menu import OKButton
 from pyface.api import clipboard
 
-# Local imports
-from dataset import DataSet
-
 
 class ArrayAdapter(TabularAdapter):
+    width = 100
+    obj_name_text = Property()
+    obj_names = List([])
 
-    model_ptr = Instance(DataSet)
-
-    # font = 'Courier 10'
-    # default_bg_color = 'yellow'
-    # default_text_color = 'red'
-    # alignment = 'right'
-    # format = '%.2f'
-    width = 70
-
-    index_text = Property
-    # index_alignment = Constant('right')
-
-    def _model_ptr_changed(self):
-        self.columns = [('Index', 'index')]
-        rows, cols = self.model_ptr.matrix.shape
-        for i in xrange(cols):
-            if self.model_ptr.variable_names:
-                self.columns.append((self.model_ptr.variable_names[i], i))
-            else:
-                self.columns.append((str(i), i))
-
-    def _get_index_text(self, name):
-        if self.model_ptr.object_names:
-            return self.model_ptr.object_names[self.row]
-        else:
-            return str(self.row)
+    def _get_obj_name_text(self, name):
+        return self.obj_names[self.row]
 
 
+matrix_editor=TabularEditor(
+    adapter=ArrayAdapter(),
+    operations=[],
+    editable=False,
+    show_titles=True,
+    multi_select=True,
+    )
 
-class MatrixViewController(Controller):
+
+class MatrixViewHandler(Handler):
     """Separate table view for the dataset matrix."""
-    # data = DelegatesTo('model', prefix='matrix')
     cp_clip = Button('Copy to clipboard')
-    ad = ArrayAdapter()
-    tab_ed=TabularEditor(
-        adapter=ad,
-        operations=[],
-        editable=False,
-        show_titles=True,
-        multi_select=True,
-        )
-
-    trait_view = View(
-        Item(name='matrix',
-             editor=tab_ed,
-             show_label=False,
-             style='readonly',
-             ),
-        Item('controller.cp_clip'),
-        title='Dataset matrix',
-        width=0.3,
-        height=0.3,
-        resizable=True,
-        buttons=[OKButton],
-        )
-
-    def init_info(self, info):
-        print("init info run")
-        self.ad.model_ptr = self.model
 
     def init(self, info):
-        print("init runt")
+        varnames = self._make_header(info.object)
+        matrix_editor.adapter.columns = varnames
+        matrix_editor.adapter.obj_names = info.object.object_names
 
-    def controller_cp_clip_changed(self, info):
+    def _make_header(self, ds):
+        if ds.variable_names:
+            varnames = []
+            if ds.object_names:
+                varnames = [('SampleName', 'obj_name')]
+            varnames += [(vn, i) for i, vn in enumerate(ds.variable_names)]
+            return varnames
+        else:
+            return [("var{}".format(col+1), col) for col in range(ds.n_cols)]
+
+    def handler_cp_clip_changed(self, info):
         tf = tempfile.TemporaryFile()
-        savetxt(tf, self.model.matrix, fmt='%.2f', delimiter='\t', newline='\n')
+        savetxt(tf, info.object.matrix, fmt='%.2f', delimiter='\t', newline='\n')
         tf.seek(0)
         txt_mat = tf.read()
         clipboard.data = txt_mat
 
 
+matrix_view = View(
+    Item(name='matrix',
+         editor=matrix_editor,
+         show_label=False,
+         style='readonly',
+         ),
+    Item('handler.cp_clip', show_label=False),
+    title='Dataset matrix',
+    width=0.3,
+    height=0.3,
+    resizable=True,
+    buttons=[OKButton],
+    handler=MatrixViewHandler(),
+    )
+
+
 if __name__ == '__main__':
-    
     from importer_main import ImporterMain
     fi = ImporterMain()
-    ds = fi.import_data('./datasets/A_labels.txt', True, True)
-    mvc = MatrixViewController(model=ds)
-    mvc.configure_traits()
+    ds = fi.import_data('./datasets/Vine/A_labels.txt', True, True)
+    ds.configure_traits(view=matrix_view)
--- src/ds_ui.py
+++ src/ds_ui.py
@@ -1,24 +1,33 @@
 
 # Enthought imports
-from traitsui.api import View, Item
+from traits.api import Button
+from traitsui.api import Handler, View, Item
 from traitsui.menu import OKButton, CancelButton
 
 # Local imports
 from dataset import DataSet
+from ds_matrix_view import matrix_view
 
-dsview = View(
-    Item(name = '_ds_id'),
-    Item(name = '_ds_name'),
-    Item(name = '_dataset_type'),
-    Item(name = 'variable_names'),
-    Item(name = 'object_names'),
-    Item(name = '_is_calculated'),
-    buttons = [OKButton, CancelButton])
+
+class DSListTabHandler(Handler):
+    show_matrix = Button('Show data matrix')
+
+    def handler_show_matrix_changed(self, info):
+        info.object.edit_traits(view=matrix_view)
 
 
 ds_list_tab = View(
-    Item(name = '_ds_name'),
-    Item(name = '_dataset_type'),
-    Item(name = 'n_rows', style = 'readonly'),
-    Item(name = 'n_cols', style = 'readonly'),
+    Item('_ds_name'),
+    Item('_dataset_type'),
+    Item('n_rows', style='readonly'),
+    Item('n_cols', style='readonly'),
+    Item('handler.show_matrix'),
+    handler=DSListTabHandler(),
     )
+
+
+if __name__ == '__main__':
+    from importer_main import ImporterMain
+    fi = ImporterMain()
+    ds = fi.import_data('./datasets/Vine/A_labels.txt', True, True)
+    ds.configure_traits(view=ds_list_tab)
