1d42e8039494aeb193beb0791e7a929c20097c6b
Thomas Graff <graff.thomas@gmail.com>
2011-11-29 12:28:51 +0100

    Cleanup freeze script

--- bbfreeze-cc.py
+++ bbfreeze-cc.py
@@ -3,26 +3,24 @@ import shutil
 
 from bbfreeze import Freezer
 
-FREEZE_VERSION = '0.5.0'
+FREEZE_VERSION = '0.6.0'
 R_VERSION = '2.11.1'
 # freeze folder
 ff = "consumercheck-" + FREEZE_VERSION
 # R folder
 rf = "R-" + R_VERSION
 
-# Remove old freeze
-shutil.rmtree(ff, ignore_errors=True)
-
-includes = ('')
-exclude3 = ('pydoc', 'pydoc_topics', 'pkg_resources', 'doctest', 'difflib',
+exclude1 = ['pydoc', 'pydoc_topics', 'pkg_resources', 'doctest', 'difflib',
             'cookielib', 'urllib', 'urllib2' 'acb', 'anydbm', 'ast', 'base64',
-            'pstats', 'TiffImagePlugin', 'uuid', 'modulefinder', 'xml', 'PIL')
-excludes = ('pydoc', 'pydoc_topics', 'doctest',
-            'TiffImagePlugin', 'modulefinder', 'xml', 'PIL')
+            'pstats', 'TiffImagePlugin', 'uuid', 'modulefinder', 'xml', 'PIL']
+
+#includes = pyface + pyface_action + traitsui + enable
+includes = []
+excludes = []
 
 freeze = Freezer(ff, includes=includes, excludes=excludes)
-freeze.addScript("src/consumercheck.py", gui_only=True)
-freeze.use_compression = 0
+freeze.addScript("src/consumercheck.py", gui_only=False)
+freeze.use_compression = False
 freeze.include_py = True
 freeze()    # starts the freezing process
 
@@ -32,14 +30,14 @@ ds_dest = os.path.abspath(os.path.join(ff, 'datasets'))
 shutil.copytree(ds_source, ds_dest)
 
 # xcopy src\R-2.11.1 consumercheck-0.5.2\R-2.11.1\ /S /Q
-r_source = os.path.abspath(os.path.join('src', rf))
-r_dest = os.path.abspath(os.path.join(ff, rf))
-shutil.copytree(r_source, r_dest)
+#r_source = os.path.abspath(os.path.join('src', rf))
+#r_dest = os.path.abspath(os.path.join(ff, rf))
+#shutil.copytree(r_source, r_dest)
 
 to_delete = [
     'QtWebKit4.dll', 'QtGui4.dll', 'PyQt4.QtGui.pyd', 'QtCore4.dll',
     'PyQt4.QtCore.pyd', 'QtNetwork4.dll', 'PyQt4.QtWebKit.pyd',
     '_ssl.pyd', 'PIL._imaging.pyd',
     ]
-for file in to_delete:
-    os.remove(os.path.abspath(os.path.join(ff, file)))
+#for file in to_delete:
+#    os.remove(os.path.abspath(os.path.join(ff, file)))
