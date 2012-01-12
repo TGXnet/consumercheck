a8910e95afaae3d964252e6fe6cfb93f2a2464ff
Thomas Graff <graff.thomas@gmail.com>
2012-01-12 13:39:40 +0100

    Add PC set working

--- src/new_plots.py
+++ src/new_plots.py
@@ -19,10 +19,8 @@ class PCPlotData(ArrayPlotData):
     # from abstract_plot_data import AbstractPlotData
 
     ds_counter = Int(0)
-    arrays = {}
 
-
-    def add_PC_set(self, values, labels_list=[], style_color=None):
+    def add_PC_set(self, values, labels=None, style_color=None):
         """Add a PC dataset with metadata"""
         
         self.ds_counter += 1
@@ -30,7 +28,9 @@ class PCPlotData(ArrayPlotData):
         for row in range(len(values)):
             dict_name = 's{}pc{}'.format(self.ds_counter,(row+1))
             self.arrays[dict_name] = values[row]
-            
+
+        return 's'+str(self.ds_counter)
+    
         
     def list_PC_sets():
         """List the id of each added dataset"""
@@ -51,15 +51,43 @@ class CCScatterPCPlot(Plot):
     # new_window(self, configure=False):
     # Convenience function that creates a window containing the Plot
     # from data_view import DataView
+    # Default behaviour is to plot first and second PC for each added dataset
+    # The __init__ will take data for the first dataset as parameters
 
-    def add_PC_set(self, values, labels):
+
+    def __init__(self, pc_matrix=None, pc_labels=None, **kwtraits):
+        data = PCPlotData()
+        super(CCScatterPCPlot, self).__init__(data, **kwtraits)
+
+
+    def add_PC_set(self, matrix, labels=None):
         """Add a PC dataset with metadata"""
+        set_id = self.data.add_PC_set(matrix, labels=None)
+        plot_name = self.plot_PC(set_id)
+
 
     def plot_PC(self, set_id, PCx=1, PCy=2):
         """Draw the points for a selected dataset and selecte PC for x and y axis"""
+        # Typical id: ('s1pc1', 's1pc2')
+        x_id = '{}pc{}'.format(set_id, PCx)
+        y_id = '{}pc{}'.format(set_id, PCy)
+        # plot definition
+        pd = (x_id, y_id)
+        # plot name
+        pn = 'plot_{}'.format(set_id)
+        rl = self.plot(pd,
+                       type='scatter',
+                       name=pn)
+        return pn
+
 
     def show_points(self, set_id, show=True):
         """Shows or hide datapoints for selected PC set"""
 
+
     def show_labels(self, set_id, show=True):
         """Shows or hide datapoint labels for selected PC set"""
+
+
+if __name__ == '__main__':
+    plot = CCScatterPCPlot()
