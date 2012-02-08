3367a3f9af5374fb2127b417e72a03d39a8529c7
Thomas Graff <graff.thomas@gmail.com>
2012-02-08 13:13:11 +0100

    Added test container and dsl mock
    Refactored and added functions.

--- src/tests/conftest.py
+++ src/tests/conftest.py
@@ -9,11 +9,17 @@
 
 # Std lib imports
 import numpy as np
-from os.path import dirname, abspath, join
+# from os.path import dirname, abspath, join
+import os.path as osp
+
+# Enthought Toolkit imports
+from traits.api import HasTraits, Instance
 
 # Local imports
 from dataset import DataSet
 from plot_pc_scatter import PCScatterPlot
+from dataset_collection import DatasetCollection
+from importer_main import ImporterMain
 
 
 def pytest_runtest_setup(item):
@@ -25,11 +31,15 @@ def pytest_funcarg__ds(request):
     return DataSet()
 
 
+def get_test_ds_path():
+    here = osp.dirname(__file__)
+    basedir = osp.dirname(here)
+    return osp.join(basedir, 'datasets')
+
+
 def pytest_funcarg__test_ds_dir(request):
     """Yield a path to the test data directory"""
-    here = dirname(__file__)
-    basedir = dirname(here)
-    return join(basedir, 'datasets')
+    return get_test_ds_path()
 
 
 def pytest_funcarg__simple_plot(request):
@@ -47,3 +57,51 @@ def pytest_funcarg__simple_plot(request):
     ## sp.add_PC_set(set1, labels=label1, color=(0.8, 0.2, 0.1, 1.0))
 
     return sp
+
+
+def make_dsl_mock():
+    dsl = DatasetCollection()
+    importer = ImporterMain()
+    tdd = get_test_ds_path()
+#    dsl.add_dataset(importer.import_data('datasets/CheeseSensoryData.txt'))
+#    dsl.add_dataset(importer.import_data('datasets/SausageSensoryData.txt', True, True))
+    dsl.add_dataset(importer.import_data(osp.join(tdd, 'Vine', 'A_labels.txt')))
+    dsl.add_dataset(importer.import_data(osp.join(tdd, 'Vine', 'C_labels.txt')))
+    dsl.add_dataset(importer.import_data(osp.join(tdd, 'Cheese', 'ConsumerLiking.txt')))
+    dsl.add_dataset(importer.import_data(osp.join(tdd, 'Cheese', 'SensoryData.txt')))
+    dsl._datasets['a_labels']._ds_name = 'Set A tull'
+    dsl._datasets['c_labels']._ds_name = 'Set C tull'
+    dsl._datasets['a_labels']._dataset_type = 'Consumer liking'
+    dsl._datasets['c_labels']._dataset_type = 'Sensory profiling'
+    dsl._datasets['consumerliking']._ds_name = 'Forbruker'
+    dsl._datasets['consumerliking']._dataset_type = 'Consumer liking'
+    dsl._datasets['sensorydata']._ds_name = 'Sensorikk'
+    dsl._datasets['sensorydata']._dataset_type = 'Sensory profiling'
+    return dsl
+
+
+def pytest_funcarg__dsl_data(request):
+    """Makes a test dataset collection"""
+    return make_dsl_mock()
+
+
+class TestContainer(HasTraits):
+    """Main frame for testing method tabs
+    """
+    test_subject = Instance(HasTraits)
+    dsl = Instance(DatasetCollection)
+
+    def _dsl_default(self):
+        return make_dsl_mock()
+
+    def _test_subject_changed(self, old, new):
+        if old is not None:
+            if hasattr(old, 'main_ui_ptr'):
+                old.main_ui_ptr = None
+        if new is not None:
+            if hasattr(new, 'main_ui_ptr'):
+                new.main_ui_ptr = self
+
+
+def pytest_funcarg__test_container(request):
+    return TestContainer()
