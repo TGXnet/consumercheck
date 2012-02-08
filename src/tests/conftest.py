9302861936d2b59ed71eb9f4698ebb5d1d74e024
Thomas Graff <graff.thomas@gmail.com>
2012-02-08 11:14:13 +0100

    Adjusted plot test script

--- src/tests/conftest.py
+++ src/tests/conftest.py
@@ -37,11 +37,12 @@ def pytest_funcarg__simple_plot(request):
     set1 = np.array([
         [-0.3, 0.4, 0.9],
         [-0.1, 0.2, 0.7],
-        [-0.1, 0.1, 0.1],
+        [-0.1, 0.3, 0.1],
+        [0.1, 0.2, 0.1],
         ])
-    label1 = ['s1pt1', 's1pt2', 's1pt3']
+    label1 = ['s1pt1', 's1pt2', 's1pt3', 's1pt4']
     expl_vars = {1:37.34, 2:9.4, 3:0.3498}
-    color = (0.8, 0.2, 0.1, 1.0)
+    color = (0.7, 0.9, 0.4, 1.0)
     sp = PCScatterPlot(set1, label1, color, expl_vars)
     ## sp.add_PC_set(set1, labels=label1, color=(0.8, 0.2, 0.1, 1.0))
 
