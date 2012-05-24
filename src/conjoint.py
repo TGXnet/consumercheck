13ff461db9d18decf1b691f77ef2753acc9f8f7d
Thomas Graff <graff.thomas@gmail.com>
2012-05-24 13:51:31 +0200

    Unix line endings and verbose flag for debug printing.
    --HG--
    branch : conjoint

--- src/conjoint.py
+++ src/conjoint.py
@@ -34,7 +34,7 @@ class RConjoint:
     """
     A wrapper around Alexandra's conjoint function in R
     """
-    
+
     def __init__(self, structure, \
                  consAtts, selected_consAtts, \
                  design, selected_designVars, \
@@ -42,33 +42,35 @@ class RConjoint:
         """
         Input:
         ======
-        
+
         type <structure>: integer (may take value 1, 2 or 3)
-        
+
         type <consAtt>: class arrayIO from statTools
-        
+
         type <selected_consAtts>: list holding strings
-        
+
         type <design>: class arrayIO from statTools
-        
+
         type <selected_designVars>: list holding strings
-        
+
         type <consLiking>: class arrayIO from statTools
-        
+
         type <consLikngTag>: string (data array tag from ConsumerCheck)
         """
-        
-        print; print '----- 1 -----'; print
-        
-        print; print 'MODEL SETTINGS' 
-        print '--------------'
-        print 'Structure =', structure
-        print 'Consumer attributes:', selected_consAtts
-        print 'Design variables:', selected_designVars
-        print 'Response:', consLikingTag
-        
-        print; print '----- 2 -----'; print
-        
+        verbose = True
+
+        if verbose:
+            print; print '----- 1 -----'; print
+
+            print; print 'MODEL SETTINGS'
+            print '--------------'
+            print 'Structure =', structure
+            print 'Consumer attributes:', selected_consAtts
+            print 'Design variables:', selected_designVars
+            print 'Response:', consLikingTag
+
+            print; print '----- 2 -----'; print
+
         # Transfer consumer attribute data from Python to R and build data 
         # frame in R space
         r['consAttMat'] = consAtts.data
@@ -77,20 +79,24 @@ class RConjoint:
         r('consum.attr <- as.data.frame(consAttMat)')
         r('colnames(consum.attr) <- consAttVars')
         r('rownames(consum.attr) <- consAttObj')
-        print(r('consum.attr'))
-        
-        print; print '----- 3 -----'; print
-        
+
+        if verbose:
+            print(r('consum.attr'))
+
+            print; print '----- 3 -----'; print
+
         r['designMat'] = design.data
         r['designVars'] = design.varNames
         r['designObj'] = design.objNames
         r('design.matr <- as.data.frame(designMat)')
         r('colnames(design.matr) <- designVars')
         r('rownames(design.matr) <- designObj')
-        print(r('design.matr'))
-        
-        print; print '----- 4 -----'; print
-        
+
+        if verbose:
+            print(r('design.matr'))
+
+            print; print '----- 4 -----'; print
+
         # Transfer odour/flavour liking data from Python to R and build data 
         # frame in R space
         r['consLikingMat'] = consLiking.data
@@ -99,142 +105,142 @@ class RConjoint:
         r('cons.liking <- as.data.frame(consLikingMat)')
         r('colnames(cons.liking) <- consLikingVars')
         r('rownames(cons.liking) <- consLikingObj')
-        
+
         # Make row and column names for class function '.residualsTable',
         # since the R function provides only a long vector instad of an array
         # with row and column names.
         self.consLiking = consLiking
-        
-                
-        print; print '----- 5 -----'; print
+
+        if verbose:
+            print; print '----- 5 -----'; print
 
         # Construct a list in R space that holds data and names of liking matrices
         rCommand_buildLikingList = 'list.consum.liking <- list(matr.liking=list({0}), names.liking=c("{1}"))'.format('cons.liking', consLikingTag)
-        print rCommand_buildLikingList
+
+        if verbose:
+            print rCommand_buildLikingList
         r(rCommand_buildLikingList)
-        
-        
-        print; print '----- 6 -----'; print
-        
+
+        if verbose:
+            print; print '----- 6 -----'; print
+
         # Construct R list with R lists of product design variables as well as
-        # consumer attributes.        
-        
+        # consumer attributes.
+
         # Construct string holding design variables that is needed for 
         # construction of rCommand_fixedFactors
         for desInd, desVar in enumerate(selected_designVars):
-            
+
             if desInd == 0:
                 selDesVarStr = '"{0}"'.format(selected_designVars[desInd])
-            
+
             else:
                 newStrPart = '"{0}"'.format(selected_designVars[desInd])
                 selDesVarStr = selDesVarStr + ',' + newStrPart
-        
-       
+
         # Construct string holding consumer attributes that is needed for 
         # construction of rCommand_fixedFactors
         for consInd, consAtt in enumerate(selected_consAtts):
-            
+
             if consInd == 0:
                 selConsAttStr = '"{0}"'.format(selected_consAtts[consInd])
-            
+
             else:
                 newStrPart = '"{0}"'.format(selected_consAtts[consInd])
                 selConsAttStr = selConsAttStr + ',' + newStrPart
-        
-            
+
         rCommand_fixedFactors = 'fixed <- list(Product=c({0}), Consumer={1})'.format(selDesVarStr, selConsAttStr)
-        print rCommand_fixedFactors
+
+        if verbose:
+            print rCommand_fixedFactors
         r(rCommand_fixedFactors)
-        print(r('fixed'))
-        
 
-        print; print '----- 7 -----'; print
+        if verbose:
+            print(r('fixed'))
 
-        
+            print; print '----- 7 -----'; print
+
         # Define which factors are random and construct R list with all
         # factors, both random and fixed. Define also response since the
         # name will be used in result tables.
         r['random'] = 'Consumer'
         r['response'] = consLikingTag
-        
+
         facs = ['Consumer']
         facs.extend(selected_designVars)
         facs.extend(selected_consAtts)
         r['facs'] = facs
-        
-        print(r('random'))
-        print(r('response'))
-        print(r('facs'))
-        
-        print; print '----- 8 -----'; print
-        
+
+        if verbose:
+            print(r('random'))
+            print(r('response'))
+            print(r('facs'))
+
+            print; print '----- 8 -----'; print
+
         rCommand_runAnalysis = 'res.gm <- conjoint(structure={0}, consum.attr=consum.attr, design.matr=design.matr, list.consum.liking=list.consum.liking, response, fixed, random, facs)'.format(structure)
-        print rCommand_runAnalysis
+
+        if verbose:
+            print rCommand_runAnalysis
         r(rCommand_runAnalysis)
-        
-        
-        print; print '----- 9 -----'; print
-        
-        print(r('res.gm'))  
-    
-    
+
+        if verbose:
+            print; print '----- 9 -----'; print
+
+            print(r('res.gm'))
+
     def randomTable(self):
         """
         Returns random table from R conjoint function.
         """
         r('randTab <- res.gm[[1]][1]')
-        
+
         randTableDict = {}
         randTableDict['data'] = r['randTab']['rand.table']
         randTableDict['colNames'] = r['colnames(res.gm$odourflavour$rand.table)']
         randTableDict['rowNames'] = r['rownames(res.gm$odourflavour$rand.table)']
-        
+
         return randTableDict
-    
-    
+
     def anovaTable(self):
         """
         Returns ANOVA table from R conjoint function.
         """
         r('anovaTab <- res.gm[[1]][2]')
-        
+
         anovaTableDict = {}
         anovaTableDict['data'] = r['anovaTab']['anova.table']
         anovaTableDict['colNames'] = r['colnames(res.gm$odourflavour$anova.table)']
         anovaTableDict['rowNames'] = r['rownames(res.gm$odourflavour$anova.table)']
 
         return anovaTableDict
-        
-        
+
     def lsmeansTable(self):
         """
         Returns LS means table from R conjoint function.
         """
         r('lsmeansTab <- res.gm[[1]][3]')
-        
+
         lsmeansTableDict = {}
         lsmeansTableDict['data'] = r['lsmeansTab']['lsmeans.table']
         lsmeansTableDict['colNames'] = r['colnames(res.gm$odourflavour$lsmeans.table)']
         lsmeansTableDict['rowNames'] = r['rownames(res.gm$odourflavour$lsmeans.table)']
-        
+
         return lsmeansTableDict
-        
-    
+
     def lsmeansDiffTable(self):
         """
         Returns table of differences between LS means from R conjoint function.
         """
         r('lsmeansDiffTab <- res.gm[[1]][4]')
-        
+
         lsmeansDiffTableDict = {}
         lsmeansDiffTableDict['data'] = r['lsmeansDiffTab']['diffs.lsmeans.table']
         lsmeansDiffTableDict['colNames'] = r['colnames(res.gm$odourflavour$diffs.lsmeans.table)']
         lsmeansDiffTableDict['rowNames'] = r['rownames(res.gm$odourflavour$diffs.lsmeans.table)']
-        
+
         return lsmeansDiffTableDict
-        
-        
+
     def residualsTable(self):
         """
         Returns residuals from R conjoint function.
@@ -243,14 +249,14 @@ class RConjoint:
         numRows, numCols = np.shape(self.consLiking.data)
         #print 'number of rows:', numRows
         #print 'number of cols:', numCols
-        
+
         r('residTab <- res.gm[[1]][5]')
-        
+
         residTableDict = {}
         residTableDict['data'] = np.reshape(r['residTab']['residuals'], \
                 (numRows, numCols))
-        
+
         residTableDict['rowNames'] = self.consLiking.objNames
         residTableDict['colNames'] = self.consLiking.varNames
-        
+
         return residTableDict
