34bf263a48562a859daa55140ca1aee0f2627461
Thomas Graff <graff.thomas@gmail.com>
2012-05-31 22:58:42 +0200

    Implemented asyncrous conjoint calculation in separate thread.
    --HG--
    branch : conjoint

--- src/conjoint_mvc.py
+++ src/conjoint_mvc.py
@@ -5,15 +5,34 @@ import sys
 import numpy as np
 
 # Enthought imports
-from traits.api import (HasTraits, Button, Enum, Instance, List, Str, DelegatesTo,
-                        Property, cached_property, on_trait_change)
+from traits.api import (HasTraits, Button, Enum, Bool, Dict, Instance, List, Str,
+                        DelegatesTo, Property, cached_property, on_trait_change)
 from traitsui.api import View, Group, Item, Spring, ModelView, CheckListEditor
-
+from traitsui.menu import (OKButton)
 
 # Local imports
 from dataset import DataSet
 from ds_matrix_view import matrix_view
-import conjoint as cj
+# import conjoint as cj
+from conjoint_machine import ConjointMachine
+
+
+
+
+class ConjointCalcState(HasTraits):
+    messages = Str()
+    is_done = Bool(False)
+
+    traits_view = View(
+        Item('messages',show_label=False, springy=True, style='custom' ),
+        Item('is_done',show_label=False, springy=True, style='readonly' ),
+        height=400,
+        width=1200,
+        resizable=True,
+        buttons=[OKButton],
+        )
+
+
 
 
 class AConjointModel(HasTraits):
@@ -32,17 +51,41 @@ class AConjointModel(HasTraits):
     # Conjoint settings
     structure = Enum(1, 2, 3)
 
-    # depends_on
-    result = Property(depends_on='sel_design_vars, sel_cons_attr_vars, structure')
+    # Conjoint calculation state
+    ccs = Instance(ConjointCalcState, ())
+    cm = Instance(ConjointMachine, ())
 
-    @cached_property
-    def _get_result(self):
-        cj_mod = cj.RConjoint(
+    # depends_on
+    # result = Property(depends_on='sel_design_vars, sel_cons_attr_vars, structure')
+    result = Dict()
+
+
+    def update_conjoint_result(self):
+        if not self.cm.run_state:
+            self.cm.run_state = self.ccs
+
+        self.ccs.edit_traits()
+        self.cm.schedule_calculation(
             self.structure,
             self.cons_attr, self.sel_cons_attr_vars,
             self.design, self.sel_design_vars,
             self.cons_liking)
-        return cj_mod
+
+
+    @on_trait_change('ccs.is_done')
+    def _result_ready(self):
+        print("Result ready")
+        self.result = self.cm.get_result()
+
+
+    # @cached_property
+    ## def _get_result(self):
+        ## cj_mod = cj.RConjoint(
+            ## self.structure,
+            ## self.cons_attr, self.sel_cons_attr_vars,
+            ## self.design, self.sel_design_vars,
+            ## self.cons_liking)
+        ## return cj_mod
 
 
 class AConjointHandler(ModelView):
@@ -55,6 +98,7 @@ class AConjointHandler(ModelView):
     # cons_attr_vars = List(['Sex', 'Age'])
     cons_attr_vars = List()
 
+    btn_calc_conjoint = Button('Calculate conjoint')
 
     def __eq__(self, other):
         return self.nid == other
@@ -74,24 +118,27 @@ class AConjointHandler(ModelView):
         self.cons_attr_vars = self.model.cons_attr.variable_names
 
 
+    def _btn_calc_conjoint_fired(self):
+        self.model.update_conjoint_result()
+
 
     def show_random(self):
-        rand_map = self.model.result.randomTable()
-        cj_ds = self.cj_res_ds_adapter(rand_map)
+        # rand_map = self.model.result.randomTable()
+        cj_ds = self.cj_res_ds_adapter(self.model.result['randomTable'])
         cj_ds._ds_name = 'ANOVA table for random effects'
         cj_ds.edit_traits(view=matrix_view)
 
 
     def show_fixed(self):
-        anova_map = self.model.result.anovaTable()
-        cj_ds = self.cj_res_ds_adapter(anova_map)
+        # anova_map = self.model.result.anovaTable()
+        cj_ds = self.cj_res_ds_adapter(self.model.result['anovaTable'])
         cj_ds._ds_name = 'ANOVA table for fixed effects'
         cj_ds.edit_traits(view=matrix_view)
 
 
     def show_means(self):
-        ls_means_map = self.model.result.lsmeansTable()
-        cj_ds = self.cj_res_ds_adapter(ls_means_map)
+        # ls_means_map = self.model.result.lsmeansTable()
+        cj_ds = self.cj_res_ds_adapter(self.model.result['lsmeansTable'])
         cj_ds._ds_name = 'LS means (main effect and interaction)'
         cj_ds.edit_traits(view=matrix_view)
 
@@ -153,6 +200,7 @@ gr_sel = Group(
             show_border=True,
             label='Model structure type',
             ),
+        Item('btn_calc_conjoint', show_label=False),
         Spring(),
         orientation='horizontal',
         ),
