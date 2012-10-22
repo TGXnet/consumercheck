b1dea406ebc26912912aadba4f9fb716f0d22a83
Thomas Graff <graff.thomas@gmail.com>
2012-10-22 13:23:15 +0200

    Blip.
    --HG--
    branch : qt_switch

--- src/tests/test_ui_pca.py
+++ src/tests/test_ui_pca.py
@@ -15,4 +15,4 @@ def test_ui_mock(plugin_mother_mock):
     # To force populating selection list
     plugin_mother_mock.ds_event = True
     with np.errstate(invalid='ignore'):
-        plugin_mother_mock.test_subject.configure_traits()
+        ui = plugin_mother_mock.test_subject.configure_traits()
