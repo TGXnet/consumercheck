f41c82724e16d0d5a4aebf9014bede3a8e35c807
Havard <havard.line@lts.no>
2012-11-05 14:41:32 +0100

    Made MainEffectsPlot work in conjoint tab Ticket: 178

--- src/plot_conjoint.py
+++ src/plot_conjoint.py
@@ -8,11 +8,15 @@ from traits.api import List, HasTraits, implements
 from enable.api import ColorTrait
 from chaco.tools.api import ZoomTool, PanTool
 
+from plugin_tree_helper import WindowLauncher
+
 
 class MainEffectsPlot(Plot):
+    pl_ref = List(WindowLauncher)
 
-    def __init__(self, conj_res, attr_name):
+    def __init__(self, conj_res, attr_name, pl_ref):
         super(MainEffectsPlot, self).__init__()
+        self.pl_ref = pl_ref
         self._adapt_conj_main_effect_data(conj_res, attr_name)
 
 
@@ -27,12 +31,10 @@ class MainEffectsPlot(Plot):
                 continue
             else:
                 exclude.append(ls_means[col] != 'NA')
-
         picker = ls_means[attr_name] != 'NA'
         
         for out in exclude:
             picker = np.logical_and(picker, np.logical_not(out))
-
         selected = ls_means[picker][[attr_name, ' Estimate ']]
         apd = ArrayPlotData()
         apd.set_data('index', [int(val) for val in selected[attr_name]])
@@ -86,8 +88,8 @@ if __name__ == '__main__':
     from tests.conftest import conj_res
     res = conj_res()
 
-    mep = MainEffectsPlot(res, 'Flavour')
+    mep = MainEffectsPlot(res, 'Sugarlevel', None)
     mep.new_window(True)
-    iap = InteractionPlot(res, 'Sex', 'Sugarlevel')
-    iap.new_window(True)
+#    iap = InteractionPlot(res, 'Sex', 'Sugarlevel')
+#    iap.new_window(True)
     print("The end")
