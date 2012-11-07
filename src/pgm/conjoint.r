09726ba032788f1d20d196acbceac4d30dfbd902
Thomas Graff <graff.thomas@gmail.com>
2012-11-07 18:50:27 +0100

    Style for redability

--- src/pgm/conjoint.r
+++ src/pgm/conjoint.r
@@ -2,7 +2,7 @@
 # performs  Conjoint Analysis for lmer object
 ##############################################################################
 
-ConjointMerge<-function(structure=1, consum.attr, design.matr, list.consum.liking, response, fixed, random, facs, corr = FALSE, alpha.rand = 0.1, alpha.fix = 0.05, plot=FALSE) {
+ConjointMerge <- function(structure=1, consum.attr, design.matr, list.consum.liking, response, fixed, random, facs, corr=FALSE, alpha.rand=0.1, alpha.fix=0.05, plot=FALSE) {
   #structure=1  (default structure) : Analysis of main effects, Random consumer effect AND interaction 
   #         between consumer and the main effects. 
   #        (Automized reduction in random part, NO reduction in fixed part).
@@ -17,87 +17,83 @@ ConjointMerge<-function(structure=1, consum.attr, design.matr, list.consum.likin
   source(paste(getwd(), "/pgm/convertToFactors.r", sep = ""))
   source(paste(getwd(), "/pgm/dataPrep.r", sep = ""))
 
-  resultFULL<-vector("list",length(response))  #the result that will be returned
+  resultFULL <- vector("list", length(response))  #the result that will be returned
 
-  data<-makeDataForConjoint(design.matr, list.consum.liking, consum.attr)
-
-  #source(paste(getwd(), "/pgm/convertToFactors.r", sep = ""))
-  #data<-convertToFactors(data,c(fixed,random))
+  data <- makeDataForConjoint(design.matr, list.consum.liking, consum.attr)
 
   #convert some of the variables specified by user to factors
-  data<-convertToFactors(data, facs)
+  data <- convertToFactors(data, facs)
 
   for(ind.resp in 1:length(response)) {
     print(paste("Calculating ", response[ind.resp],"...", sep=" "))
-    model<-createLMERmodel(structure, data, response[ind.resp], fixed, random, corr)
+    model <- createLMERmodel(structure, data, response[ind.resp], fixed, random, corr)
 
     #check if reduction of the fixed part is required
     if(structure==1 || structure==2)
-      isFixReduce<-FALSE
+      isFixReduce <- FALSE
     else
-      isFixReduce<-TRUE
-    isRandReduce<-TRUE
-    isLsmeans<-TRUE
+      isFixReduce <- TRUE
+    isRandReduce <- TRUE
+    isLsmeans <- TRUE
 
     #check if there are correlations between intercepts and slopes
-    checkCorr<-function(model) {
-      corr.intsl<-FALSE
-      lnST<-length(model@ST)
+    checkCorr <- function(model) {
+      corr.intsl <- FALSE
+      lnST <- length(model@ST)
       for(i in 1:lnST) {
         if(nrow(model@ST[[i]])>1)
-          corr.intsl<-TRUE
+          corr.intsl <- TRUE
       }
       return(corr.intsl)
     }
 
     if(checkCorr(model))
-      isRandReduce<-FALSE
+      isRandReduce <- FALSE
 
     #print(model)
     #print(data)
     #print(model)
 
-    #t<-totalAnovaRandLsmeans(model, data,  alpha.rand = 0.05, alpha.fix = 0.05, isFixReduce = FALSE, isRandReduce = FALSE, isTotal=TRUE, isAnova=FALSE, isRand=FALSE, isLSMEANS=FALSE, test.effs=NULL, plot=FALSE)
+    t <- totalAnalysis(model, data, isFixReduce=isFixReduce, isRandReduce=isRandReduce, isLsmeans=isLsmeans, plot=plot, alpha.rand=alpha.rand, alpha.fix=alpha.fix)
 
-    t<-totalAnalysis(model, data, isFixReduce=isFixReduce, isRandReduce=isRandReduce, isLsmeans=isLsmeans , plot=plot, alpha.rand=alpha.rand, alpha.fix=alpha.fix)
-
-    getFormula<-function(model, withRand=TRUE) {
-      fmodel<-formula(model)
-      terms.fm<-attr(terms(fmodel),"term.labels")
-      ind.rand.terms<-which(unlist(lapply(terms.fm,function(x) substring.location(x, "|")$first))!=0)
-      terms.fm[ind.rand.terms]<-unlist(lapply(terms.fm[ind.rand.terms],function(x) paste("(",x,")",sep="")))
-      fm<-paste(fmodel)
+    getFormula <- function(model, withRand=TRUE) {
+      fmodel <- formula(model)
+      terms.fm <- attr(terms(fmodel),"term.labels")
+      ind.rand.terms <- which(unlist(lapply(terms.fm, function(x) substring.location(x, "|")$first))!=0)
+      terms.fm[ind.rand.terms] <- unlist(lapply(terms.fm[ind.rand.terms], function(x) paste("(",x,")",sep="")))
+      fm <- paste(fmodel)
       if(withRand)
-        fm[3]<-paste(terms.fm,collapse=" + ")
+        fm[3] <- paste(terms.fm,collapse=" + ")
       else
-        fm[3]<-paste(terms.fm[-ind.rand.terms],collapse=" + ")
+        fm[3] <- paste(terms.fm[-ind.rand.terms], collapse=" + ")
       return(fm)
     }
 
-    fillresult<-function(t) {
-      result<-NULL
-      result$rand.table<-t$rand.table
-      result$anova.table<-t$anova.table
-      result$lsmeans.table<-t$lsmeans.table
-      result$diffs.lsmeans.table<-t$diffs.lsmeans.table
-      fm<-getFormula(t$model, withRand=FALSE)
+    fillresult <- function(t) {
+      result <- NULL
+      result$rand.table <- t$rand.table
+      result$anova.table <- t$anova.table
+      result$lsmeans.table <- t$lsmeans.table
+      result$diffs.lsmeans.table <- t$diffs.lsmeans.table
+      fm <- getFormula(t$model, withRand=FALSE)
       if(fm[3]=="")
-        res<-lm(as.formula(paste(fm[2],fm[1],1, sep="")), data=data)$residuals
+        res <- lm(as.formula(paste(fm[2], fm[1], 1, sep="")), data=data)$residuals
       else
-        res<-lm(as.formula(paste(fm[2],fm[1],fm[3], sep="")), data=data)$residuals
-      result$residuals<-res
+        res <- lm(as.formula(paste(fm[2], fm[1], fm[3], sep="")), data=data)$residuals
+      result$residuals <- res
       return(result)
     }
 
-    resultFULL[[ind.resp]]<-fillresult(t)
+    resultFULL[[ind.resp]] <- fillresult(t)
   }
 
-  names(resultFULL)<-response
+  names(resultFULL) <- response
   return(resultFULL)
 }
 
+
 # Data merge implemented in python
-ConjointNoMerge<-function(structure=1, data, response, fixed, random, facs, corr = FALSE, alpha.rand = 0.1, alpha.fix = 0.05, plot=FALSE) {
+ConjointNoMerge <- function(structure=1, data, response, fixed, random, facs, corr=FALSE, alpha.rand=0.1, alpha.fix=0.05, plot=FALSE) {
   #structure=1  (default structure) : Analysis of main effects, Random consumer effect AND interaction 
   #         between consumer and the main effects. 
   #        (Automized reduction in random part, NO reduction in fixed part).
@@ -110,85 +106,79 @@ ConjointNoMerge<-function(structure=1, data, response, fixed, random, facs, corr
 
   source(paste(getwd(), "/pgm/createLMERmodel.r", sep = ""))
   source(paste(getwd(), "/pgm/convertToFactors.r", sep = ""))
-  source(paste(getwd(), "/pgm/dataPrep.r", sep = ""))  
   
   #the result that will be returned
-  resultFULL<-vector("list",length(response))
-
-  #data<-makeDataForConjoint(design.matr, list.consum.liking, consum.attr)
-
-  #source(paste(getwd(), "/pgm/convertToFactors.r", sep = ""))
-  #data<-convertToFactors(data,c(fixed,random))
+  resultFULL <- vector("list",length(response))
 
   #convert some of the variables specified by user to factors
-  data<-convertToFactors(data, facs)
+  data <- convertToFactors(data, facs)
 
  #print(data)
 
   for(ind.resp in 1:length(response)) {
-    print(paste("Calculating ", response[ind.resp],"...", sep=" "))
-    model<-createLMERmodel(structure, data, response[ind.resp], fixed, random, corr)
+    print(paste("Calculating ", response[ind.resp], "...", sep=" "))
+    model <- createLMERmodel(structure, data, response[ind.resp], fixed, random, corr)
     #check if reduction of the fixed part is required
     if(structure==1 || structure==2)
-      isFixReduce<-FALSE
+      isFixReduce <- FALSE
     else
-      isFixReduce<-TRUE
-    isRandReduce<-TRUE
-    isLsmeans<-TRUE
-  
+      isFixReduce <- TRUE
+    isRandReduce <- TRUE
+    isLsmeans <- TRUE
+
     #check if there are correlations between intercepts and slopes
-    checkCorr<-function(model) {
-      corr.intsl<-FALSE
-      lnST<-length(model@ST)
+    checkCorr <- function(model) {
+      corr.intsl <- FALSE
+      lnST <- length(model@ST)
       for(i in 1:lnST) {
         if(nrow(model@ST[[i]])>1)
-          corr.intsl<-TRUE
+          corr.intsl <- TRUE
       }
       return(corr.intsl)
     }
 
     if(checkCorr(model))
-      isRandReduce<-FALSE
+      isRandReduce <- FALSE
 
     #print(model)
     #print(data)
     #print(model)
 
-    #t<-totalAnovaRandLsmeans(model, data,  alpha.rand = 0.05, alpha.fix = 0.05, isFixReduce = FALSE, isRandReduce = FALSE, isTotal=TRUE, isAnova=FALSE, isRand=FALSE, isLSMEANS=FALSE, test.effs=NULL, plot=FALSE)
+    t <- totalAnalysis(model, data, isFixReduce=isFixReduce, isRandReduce=isRandReduce, isLsmeans=isLsmeans, plot=plot, alpha.rand=alpha.rand, alpha.fix=alpha.fix)
 
-    t<-totalAnalysis(model, data, isFixReduce=isFixReduce, isRandReduce=isRandReduce, isLsmeans=isLsmeans , plot=plot, alpha.rand=alpha.rand, alpha.fix=alpha.fix)
 
-    getFormula<-function(model, withRand=TRUE) {
-      fmodel<-formula(model)
-      terms.fm<-attr(terms(fmodel),"term.labels")
-      ind.rand.terms<-which(unlist(lapply(terms.fm,function(x) substring.location(x, "|")$first))!=0)
-      terms.fm[ind.rand.terms]<-unlist(lapply(terms.fm[ind.rand.terms],function(x) paste("(",x,")",sep="")))
-      fm<-paste(fmodel)
+    getFormula <- function(model, withRand=TRUE) {
+      fmodel <- formula(model)
+      terms.fm <- attr(terms(fmodel), "term.labels")
+      ind.rand.terms <- which(unlist(lapply(terms.fm,function(x) substring.location(x, "|")$first))!=0)
+      terms.fm[ind.rand.terms] <- unlist(lapply(terms.fm[ind.rand.terms], function(x) paste("(",x,")",sep="")))
+      fm <- paste(fmodel)
       if(withRand)
-        fm[3]<-paste(terms.fm,collapse=" + ")
+        fm[3] <- paste(terms.fm, collapse=" + ")
       else
-        fm[3]<-paste(terms.fm[-ind.rand.terms],collapse=" + ")
+        fm[3] <- paste(terms.fm[-ind.rand.terms], collapse=" + ")
       return(fm)
     }
-  
-    fillresult<-function(t) {
-      result<-NULL
-      result$rand.table<-t$rand.table
-      result$anova.table<-t$anova.table
-      result$lsmeans.table<-t$lsmeans.table
-      result$diffs.lsmeans.table<-t$diffs.lsmeans.table
-      fm<-getFormula(t$model, withRand=FALSE)
+
+
+    fillresult <- function(t) {
+      result <- NULL
+      result$rand.table <- t$rand.table
+      result$anova.table <- t$anova.table
+      result$lsmeans.table <- t$lsmeans.table
+      result$diffs.lsmeans.table <- t$diffs.lsmeans.table
+      fm <- getFormula(t$model, withRand=FALSE)
       if(fm[3]=="")
-        res<-lm(as.formula(paste(fm[2],fm[1],1, sep="")), data=data)$residuals
+        res <- lm(as.formula(paste(fm[2],fm[1], 1, sep="")), data=data)$residuals
       else
-        res<-lm(as.formula(paste(fm[2],fm[1],fm[3], sep="")), data=data)$residuals
-      result$residuals<-res
+        res <- lm(as.formula(paste(fm[2], fm[1], fm[3], sep="")), data=data)$residuals
+      result$residuals <- res
       return(result)
     }
 
-    resultFULL[[ind.resp]]<-fillresult(t)
+    resultFULL[[ind.resp]] <- fillresult(t)
   }
 
-  names(resultFULL)<-response
+  names(resultFULL) <- response
   return(resultFULL)
 }
