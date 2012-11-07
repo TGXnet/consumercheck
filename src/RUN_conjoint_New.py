aa2a8e81ad02e890db8c8105bcd3d2e6d935f8a8
Thomas Graff <graff.thomas@gmail.com>
2012-11-07 15:58:38 +0100

    Add Olivers implementation of conjoint merge in python.
    I will get this to work and move the new functionality into conjoint_machine.py

---
+++ src/RUN_conjoint_New.py
@@ -0,0 +1,80 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Apr 19 14:45:17 2012
+
+@author: oliver.tomic
+
+@purpose:
+Run Conjoint class.
+ - uses Alexandra's R package MixMod
+ - accessed via pyper
+"""
+
+# Import necessary modules
+import conjoint_one as cj
+import statTools as st
+
+
+#==============================================================================
+# Load data that will be used in conjoint function (God Morgen TINE)
+#==============================================================================
+
+# Consumer attributes
+consAttr = st.arrayIO('consumerAttributes.txt')
+
+# Three different types of liking
+odflLike = st.arrayIO('odour-flavour_liking.txt')
+consistencyLike = st.arrayIO('consistency_liking.txt')
+overallLike = st.arrayIO('overall_liking.txt')
+
+# Design
+designVar = st.arrayIO('design.txt')
+
+
+#==============================================================================
+# Simulate selected settings from conjoint GUI
+#==============================================================================
+
+# From radio button box choose model structure (possible choices are 1, 2 or 3)
+selected_structure = 2
+
+# From check box list submit selected consumer attributes.
+# They are found in consAtt.varNames
+#selected_consAttr = ['Sex', 'Age']
+selected_consAttr = ['Sex']
+
+# From check box list submit selected design variables. 
+# They are found in design.varNames
+selected_designVar = ['Flavour', 'Sugarlevel']
+
+# From check box list submit selected consumer liking data sets
+selected_consLiking = [odflLike, consistencyLike, overallLike]
+selected_consLiking_tags = ['odourflavour', 'consistency', 'overall']
+
+
+
+#==============================================================================
+# Submit settings to conjoint class
+#==============================================================================
+
+# Temporary used consumer liking
+consLiking = odflLike
+consLikingTag = 'odourflavour'
+
+# Call conjoint class in Python to do analysis
+conjMod = cj.RConjoint(selected_structure, \
+                       consAttr, selected_consAttr, \
+                       designVar, selected_designVar, \
+                       consLiking, consLikingTag)
+
+
+finalDataArr = conjMod.inputData()
+
+randomTable = conjMod.randomTable()
+anovaTable = conjMod.anovaTable()
+lsmeansTable = conjMod.lsmeansTable()
+lsmeansDiffTable = conjMod.lsmeansDiffTable()
+residualsTable = conjMod.residualsTable()
+infoDict = conjMod.infoDict()
+
+
