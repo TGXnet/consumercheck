6349972b5ab71b6acc1e932b5fd61ed62e7f61d3
Thomas Graff <graff.thomas@gmail.com>
2012-11-15 10:20:06 +0100

    Update to version 0.7.5

--- make_wix.bat
+++ make_wix.bat
@@ -1,8 +1,8 @@
 rem *** Harvest files
-heat.exe dir consumercheck-0.7.0 -gg -srd -sreg -scom -cg CcFiles -dr CCROOTDIR -ke -generate components -out ccfiles.wxs
+heat.exe dir consumercheck-0.7.5 -gg -srd -sreg -scom -cg CcFiles -dr CCROOTDIR -ke -generate components -out ccfiles.wxs
 
 rem *** Run compiler
 candle ccfiles.wxs consumercheck.wxs
 
 rem *** Run linker
-light -b consumercheck-0.7.0 ccfiles.wixobj consumercheck.wixobj -out consumercheck-0.7.0.msi
+light -b consumercheck-0.7.5 ccfiles.wixobj consumercheck.wixobj -out consumercheck-0.7.5.msi
