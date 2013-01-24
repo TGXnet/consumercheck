40f84ef5a26d05c868b5387320cf67945dc4287c
Thomas Graff <graff.thomas@gmail.com>
2013-01-24 18:55:52 +0100

    Cosmetic plot fixes

--- src/plot_base.py
+++ src/plot_base.py
@@ -7,6 +7,9 @@ from chaco.api import Plot
 class PlotBase(Plot):
     # The function to call if the plot i double clicked
     ld_action_func = Callable()
+    # Add extra room for y axis
+    padding_left = 75
+
 
     def add_left_down_action(self, left_down_action_func):
         self.ld_action_func = left_down_action_func
