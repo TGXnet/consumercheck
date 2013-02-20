96ec51eec502ddf1fd85647f44b9da21ae173a6f
Thomas Graff <graff.thomas@gmail.com>
2013-02-20 15:02:22 +0100

    Make dataset more migration friendly

--- src/dataset_ng.py
+++ src/dataset_ng.py
@@ -1,8 +1,11 @@
 
+# Std lib imports
+import warnings
+import itertools as _itr
+
 # Num lib imports
 import numpy as _np
 import pandas as _pd
-import itertools as _itr
 
 # ETS imports
 import traits.api as _traits
@@ -14,12 +17,16 @@ DS_TYPES = ['Design variable',
             'Consumer characteristics']
 
 
+def deprecation(message):
+    warnings.warn(message, DeprecationWarning, stacklevel=2)
+
+
 class DataSet(_traits.HasTraits):
     '''Dataset for holding one matrix
 
     and associated metadata
     '''
-    matrix = _traits.Instance(_pd.DataFrame)
+    _matrix = _traits.Instance(_pd.DataFrame)
 
     _id = _traits.Int()
     id = _traits.Property()
@@ -36,6 +43,44 @@ class DataSet(_traits.HasTraits):
     missing_data = _traits.Property(_traits.Bool)
 
 
+    # FIXME: For backward compability
+    n_cols = _traits.Property()
+    n_rows = _traits.Property()
+    variable_names = _traits.Property()
+    object_names = _traits.Property()
+    values = _traits.Property()
+    matrix = _traits.Property()
+
+
+    def _get_matrix(self):
+        deprecation("Use values instead")
+        return self._matrix.values
+
+
+    def _get_values(self):
+        return self._matrix.values
+
+
+    def _set_matrix(self, value):
+        self._matrix = value
+
+
+    def _get_n_cols(self):
+        return self._matrix.shape[1]
+
+
+    def _get_n_rows(self):
+        return self._matrix.shape[0]
+
+
+    def _get_variable_names(self):
+        return list(self._matrix.columns)
+
+
+    def _get_object_names(self):
+        return list(self._matrix.index)
+
+
     def __id_default(self):
         return DataSet.new_id()
 
@@ -45,8 +90,7 @@ class DataSet(_traits.HasTraits):
 
 
     def _get_missing_data(self):
-        return _np.any(_np.isnan(self.matrix.values))
-    
+        return _np.any(_np.isnan(self._matrix.values))
 
 
 if __name__ == '__main__':
