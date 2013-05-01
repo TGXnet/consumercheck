066b899f4c816edc4b1197cee2f9c65e95912fac
Thomas Graff <graff.thomas@gmail.com>
2013-05-01 16:04:20 +0200

    Fix testConjoint.r script

--- src/rsrc/testConjoint.r
+++ src/rsrc/testConjoint.r
@@ -2,28 +2,30 @@
 ############## test Conjoint Analysis Program (conjoint.r) ##############
 #########################################################################
 
+#### Set paths
+orig_WD <- getwd()
+# Working Directory
+WD <- dirname(orig_WD)
+setwd(WD)
+# Where the testdata is to be found
+# DD <- file.path(WD, "datasets", "HamData")
+DD <- "/home/thomas/TGXnet/Prosjekter/2009-13-ConsumerCheck/Conjoint/ConjointConsumerCheck_2012-04-27/data preparation/ham data"
 
-#### change here the path
-# dir<-"C:/alku/projects/Conjoint MixMod/Conjoint versions/ConjointConsumerCheck_27_02_2012"
-dir<-"C:/Users/PC/My Documents/CCDev/ConjointConsumerCheck_27_04_2012"
-setwd(dir)
-
-
+# Load libraries
 # install.packages(paste(getwd(),"/MixMod.zip", sep=""), repos = NULL)
 library(MixMod)
 library(lme4)
 library(Hmisc)
-source(paste(getwd(),"/rsrc/conjoint.r",sep=""))
+source(file.path(WD, "rsrc", "conjoint.r"))
 
 ###########################################################################
 # example from the book: Ham data
 ###########################################################################
 
-
 #ham<-read.csv(paste(getwd(),"/data/ham.csv",sep=""), sep = ";")
-consum.attr<-read.table(file=paste(getwd(),"/data preparation/ham data/consumAttr.txt",sep=""),header=TRUE)
-design.matr<-read.table(file=paste(getwd(),"/data preparation/ham data/designMatr.txt",sep=""),header=TRUE)
-consumer.liking<-read.table(file=paste(getwd(),"/data preparation/ham data/consumLike.txt",sep=""),header=TRUE, check.names=FALSE)
+consum.attr<-read.table(file=file.path(DD, "consumAttr.txt"), header=TRUE)
+design.matr<-read.table(file=file.path(DD, "designMatr.txt") ,header=TRUE)
+consumer.liking<-read.table(file=file.path(DD, "consumLike.txt"), header=TRUE, check.names=FALSE)
 list.consum.liking<-list(matr.liking=list(consumer.liking), names.liking=c("informed.liking"))
 
 fixed<-list(Product=c("Product", "Information"), Consumer="Kjonn")
@@ -31,29 +33,28 @@ random<-c("Consumer")
 response<-c("informed.liking")
 facs<-c("Consumer","Product","Information","Kjonn")
 
-res.ham<-conjoint(structure=3, consum.attr=consum.attr, design.matr=design.matr, list.consum.liking=list.consum.liking, response, fixed, random, facs)
+res.ham<-ConjointMerge(structure=3, consum.attr=consum.attr, design.matr=design.matr, list.consum.liking=list.consum.liking, response, fixed, random, facs)
 
 
 ###########################################################################
 # example: gm data 
 ###########################################################################
 # Load liking data (here we have three of them)
-odfl.liking <- read.table(file=paste(getwd(),"/data preparation/gm data/odour-flavour_liking.txt", sep=""), header=TRUE, check.names=FALSE)
-consistency.liking <- read.table(file=paste(getwd(),"/data preparation/gm data/consistency_liking.txt", sep=""), header=TRUE, check.names=FALSE)
-overall.liking <- read.table(file=paste(getwd(),"/data preparation/gm data/overall_liking.txt", sep=""), header=TRUE, check.names=FALSE)
+#odfl.liking <- read.table(file=paste(getwd(),"/data preparation/gm data/odour-flavour_liking.txt", sep=""), header=TRUE, check.names=FALSE)
+#consistency.liking <- read.table(file=paste(getwd(),"/data preparation/gm data/consistency_liking.txt", sep=""), header=TRUE, check.names=FALSE)
+#overall.liking <- read.table(file=paste(getwd(),"/data preparation/gm data/overall_liking.txt", sep=""), header=TRUE, check.names=FALSE)
 
 # Load consumer attributes
-consum.attr <- read.table(file=paste(getwd(),"/data preparation/gm data/consumerAttributes.txt", sep=""), header=TRUE)
+#consum.attr <- read.table(file=paste(getwd(),"/data preparation/gm data/consumerAttributes.txt", sep=""), header=TRUE)
 
 # Load design matrix
-design.matr <- read.table(file=paste(getwd(),"/data preparation/gm data/design.txt", sep=""), header=TRUE)
+#design.matr <- read.table(file=paste(getwd(),"/data preparation/gm data/design.txt", sep=""), header=TRUE)
 
-list.consum.liking<-list(matr.liking=list(odfl.liking, consistency.liking, overall.liking), names.liking=c("odourflavour", "consistency", "overall"))
+#list.consum.liking<-list(matr.liking=list(odfl.liking, consistency.liking, overall.liking), names.liking=c("odourflavour", "consistency", "overall"))
 
-fixed<-list(Product=c("Flavour", "Sugarlevel"), Consumer="Sex")
-random<-c("Consumer")
-response<-c("odourflavour", "consistency", "overall")
-facs<-c("Consumer","Flavour","Sugarlevel","Sex")
+#fixed<-list(Product=c("Flavour", "Sugarlevel"), Consumer="Sex")
+#random<-c("Consumer")
+#response<-c("odourflavour", "consistency", "overall")
+#facs<-c("Consumer","Flavour","Sugarlevel","Sex")
  
-res.gm<-conjoint(structure=3, consum.attr=consum.attr, design.matr=design.matr, list.consum.liking=list.consum.liking, response, fixed, random, facs)
-
+#res.gm<-conjoint(structure=3, consum.attr=consum.attr, design.matr=design.matr, list.consum.liking=list.consum.liking, response, fixed, random, facs)
