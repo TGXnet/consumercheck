64622f7841d4e56a66e55614397fa84e135a8d5e
Thomas Graff <graff.thomas@gmail.com>
2013-07-10 22:33:21 +0200

    Updated exception GUI dialog.
    But this is still not working in CC.

--- src/exception_handler.py
+++ src/exception_handler.py
@@ -1,6 +1,7 @@
 
 # Std lib imports
 import logging
+# logging.basicConfig()
 logger = logging.getLogger('tgxnet.nofima.cc.'+__name__)
 import sys
 import traceback
@@ -10,43 +11,67 @@ from traits.api import HasTraits, Button, Str, push_exception_handler
 from traitsui.api import View, Item
 from traitsui.menu import OKButton
 
+# Local imports
+import cc_config as conf
+
 
 class ErrorDialog(HasTraits):
-    message = Str('Something went wrong, check logfile for details')
+    message = Str()
+    def _message_default(self):
+        msg = """
+Something went wrong! :-(
+Details can be found in the log file.
+The logfile is:
+{0}
+
+The program may or may not continue to work after this.
+""".format(conf.log_file_url())
+        return msg
+
     traits_view=View(
         Item('message', style='readonly', show_label=False, springy=True),
+        title="Error",
         buttons=[OKButton],
         resizable=True,
         width=300,
         height=150,
         )
 
+# State variable to indicate whether the error dialoge have been shown or not
+shown = False
+
 # Exception handler
-def tgx_exception_handler(object, trait_name, old, new):
+def tgx_exception_handler(obj, trait_name, old, new):
     """ Logs any exceptions generated in a trait notification handler.
     """
+    global shown
+
     # When the stack depth is too great, the logger can't always log the
     # message. Make sure that it goes to the console at a minimum:
     excp_class, excp = sys.exc_info()[:2]
     if ((excp_class is RuntimeError) and
         (excp.args[0] == 'maximum recursion depth exceeded')):
-        sys.__stderr__.write( 'Exception occurred in traits notification '
+        trace_text = ''.join(traceback.format_exception(*sys.exc_info()))
+        sys.__stderr__.write(
+            'Exception occurred in traits notification '
             'handler for object: %s, trait: %s, old value: %s, '
-            'new value: %s.\n%s\n' % ( object, trait_name, old, new,
-            ''.join( traceback.format_exception( *sys.exc_info() ) ) ) )
+            'new value: %s.\n%s\n' % (obj, trait_name, old, new, trace_text))
 
-    ## try:
-    ##     logger.exception(
-    ##         'Exception occurred in traits notification handler for '
-    ##         'object: %s, trait: %s, old value: %s, new value: %s' %
-    ##         ( object, trait_name, old, new ) )
-    ## except Exception:
-    ##     # Ignore anything we can't log the above way:
-    ##     pass
+    try:
+        logger.exception(
+            'Exception occurred in traits notification handler for '
+            'object: %s, trait: %s, old value: %s, new value: %s' %
+            ( obj, trait_name, old, new ) )
+    except Exception:
+        # Ignore anything we can't log the above way:
+        pass
 
-
-    # FIXME: Make sure only one is open at the time
     print("Something went wrong")
+    # FIXME: Make sure only one is open at the time
+    if not shown:
+        ed = ErrorDialog()
+        ed.edit_traits(kind='modal')
+        shown = True
 
 
 if __name__ == '__main__':
