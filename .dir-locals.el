b02de57f002d09fbf3ffe2f4acc8ed5cdd7021f1
Thomas Graff <graff.thomas@gmail.com>
2013-10-31 06:45:47 +0100

    Add Emacs settigs file

---
+++ .dir-locals.el
@@ -0,0 +1,9 @@
+;;; Directory Local Variables
+;;; For more information see (info "(emacs) Directory Variables")
+
+((nil
+  (ffip-project-file . ".hg")
+  (ffip-project-root . "."))
+ (python-mode
+  (elpy-project-root . "/home/thomas/.virtualenvs/cc_main/")))
+
