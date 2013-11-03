bcfc503f263a2cef32466ecdc478485103cd76c6
Thomas Graff <graff.thomas@gmail.com>
2013-11-03 23:10:11 +0100

    Remove unused functions

--- src/plot_conjoint.py
+++ src/plot_conjoint.py
@@ -34,82 +34,10 @@ class MainEffectsPlot(DataView):
         self.p_value = self._get_p_value(conj_res.anovaTable, attr_name)
         self.average = conj_res.meanLiking
 
-        # self._create_plot()
         self._make_plot_frame()
         self._add_lines()
         self._add_ci_bars()
         self._add_avg_line()
-
-
-    def _adapt_conj_main_effect_data(self, conj_res, attr_name):
-        """FIXME: Can this bee extracted as an utility function
-        that will return an result object but only with the needed values?
-        """
-        ls_means = conj_res.lsmeansTable['data']
-        ls_means_labels = conj_res.lsmeansTable['rowNames']
-        cn = list(conj_res.lsmeansTable['colNames'])
-        nli = cn.index('Estimate')
-        cn = cn[:nli]
-
-        # The folowing logic is about selecting rows from the result sets.
-        # The picker i an boolean vector that selects the rows i will plot.
-
-        # First; select all rows where the given attribute have an index value
-        picker = ls_means[attr_name] != 'NA'
-
-        # Then; exclude all the interaction rows where the given attribute
-        # is a part of the interaction
-        exclude = [ls_means[col] != 'NA' for col in cn if col != attr_name]
-        for out in exclude:
-            picker = np.logical_and(picker, np.logical_not(out))
-
-        # Use the boolean selection vector to select the wanted rows from the result set
-        selected = ls_means[picker]
-        selected_labels = ls_means_labels[picker]
-
-        self.ls_label_names = []
-        self.ls_label_pos = []
-        for i, pl in enumerate(selected_labels):
-            self.ls_label_names.append(pl)
-            self.ls_label_pos.append(i + 1)
-
-        # Create a longer index line for average line
-        idx = [int(val) for val in selected[attr_name]]
-        span = idx[-1] - idx[0]
-        avg_idx = [idx[0] - span, idx[-1] + span]
-
-        self.apd = ArrayPlotData()
-        self.apd.set_data('index', idx)
-        self.apd.set_data('avg_index', avg_idx)
-        self.apd.set_data('values', [float(val) for val in selected[' Estimate ']])
-        self.apd.set_data('ylow', [float(val) for val in selected[' Lower CI ']])
-        self.apd.set_data('yhigh', [float(val) for val in selected[' Upper CI ']])
-        self.apd.set_data('average', [conj_res.meanLiking for
-                                      val in selected[attr_name]])
-        # self.data = self.apd
-
-
-        # Get p value for attribute
-        # Before Conjoint update from 2013-03-18
-        ## anova_values = conj_res.anovaTable['data']
-        ## anova_names = conj_res.anovaTable['rowNames']
-        ## picker = anova_names == attr_name
-        ## p_value = anova_values[picker, 3][0]
-        ## self.p_value = p_value
-
-        anova_values = conj_res.anovaTable['data']
-        anova_names = conj_res.anovaTable['rowNames']
-        picker = anova_names == attr_name
-        # p_value = anova_values[picker, 3][0]
-        if isinstance(picker, bool):
-            picker = np.array([picker])
-        var_row = anova_values[picker]
-        p_str = var_row['Pr(>F)'][0]
-        try:
-            p_value = float(p_str)
-        except ValueError:
-            p_value = 0.0
-        self.p_value = p_value
 
 
     def _get_p_value(self, anova_table, attr_name):
@@ -130,7 +58,6 @@ class MainEffectsPlot(DataView):
         except ValueError:
             p_value = 0.0
         return p_value
-
 
 
     def _make_plot_frame(self):
@@ -234,93 +161,8 @@ class MainEffectsPlot(DataView):
         self.add(plot_average)
 
 
-
-
-
     def mk_ads(self, name):
         return ArrayDataSource(self.plot_data.mat[name].values)
-
-
-    def _create_plot(self):
-        #Prepare data for plots
-
-        x = self.mk_ads('index')
-        xx = ArrayDataSource(self.apd['avg_index'])
-        y = self.mk_ads('values')
-        ylow = self.mk_ads('ylow')
-        yhigh = self.mk_ads('yhigh')
-        yaverage = self.mk_ads('average')
-        
-        index_mapper = LinearMapper(
-            range=DataRange1D(x, tight_bounds=False, margin=0.05))
-        value_mapper = LinearMapper(
-            range=DataRange1D(ylow, yhigh, tight_bounds=False))
-        
-        #Create vertical bars to indicate confidence interval
-        y_name='CIbars'
-        plot_err = ErrorBarPlot(
-            index=x, index_mapper=index_mapper,
-            value_low = ylow, value_high = yhigh,
-            value_mapper = value_mapper,
-            name=y_name,
-            border_visible = True)
-
-        #Append label and grid
-        x_axis = LabelAxis(
-            plot_err, name=y_name,
-            orientation="bottom",
-            tick_weight=1, tick_label_rotate_angle = 90,
-            labels=self.ls_label_names,
-            positions = self.ls_label_pos)
-
-        y_axis = PlotAxis(
-            orientation='left', title= '',
-            mapper=plot_err.value_mapper,
-            component=plot_err)
-
-        plot_err.underlays.append(x_axis)
-        plot_err.underlays.append(y_axis)
-        add_default_grids(plot_err)
-        
-        #Create averageplot
-        y_name='average'
-        plot_y_average = LinePlot(
-            index=xx, index_mapper=index_mapper,
-            value=yaverage, value_mapper=value_mapper,
-            name=y_name,
-            color='green')
-
-        #Create lineplot
-        y_name='line'
-        plot_line = LinePlot(
-            index=x, index_mapper=index_mapper,
-            value=y, value_mapper=value_mapper,
-            name=y_name)
-        
-        #Create ScatterPlot
-        y_name='scatter'
-        plot_scatter = ScatterPlot(
-            index=x, index_mapper=index_mapper,
-            value=y, value_mapper=value_mapper,
-            name=y_name,
-            color="blue", marker_size=5)
-
-        zoom = ZoomTool(plot_err, tool_mode="box", always_on=False)
-        plot_err.tools.append(PanTool(plot_err))
-        plot_err.overlays.append(zoom)
-        self.add(plot_y_average, plot_err ,plot_line, plot_scatter)
-
-        # Set border color
-        self.border_width = 10
-
-        if self.p_value < 0.001:
-            self.border_color = (1.0, 0.0, 0.0, 0.8)
-        elif self.p_value < 0.01:
-            self.border_color = (0.0, 1.0, 0.0, 0.8)
-        elif self.p_value < 0.05:
-            self.border_color = (1.0, 1.0, 0.0, 0.8)
-        else:
-            self.border_color = (0.5, 0.5, 0.5, 0.8)
 
 
     def export_image(self, fname, size=(800,600)):
