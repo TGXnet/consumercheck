a5aa888bb7a180b45208e27690c98264406ce89e
Thomas Graff <graff.thomas@gmail.com>
2013-11-18 09:43:36 +0100

    Update runtime requirements

--- src/tests/runtime_env_test.py
+++ src/tests/runtime_env_test.py
@@ -9,71 +9,75 @@ import pytest
 # http://aima.cs.berkeley.edu/python/utils.html
 #
 # Module version format
+# Module Version Numbers
 # http://www.python.org/dev/peps/pep-0396/
+# Changing the version comparison module in Distutils
+# http://www.python.org/dev/peps/pep-0386/
+# PEP 427: Wheel format?
 #
-# from distutils.version import StrictVersion
-#
-import pkg_resources
+import sys
+from pkg_resources import get_distribution as GD
+from pkg_resources import parse_version as V
 
 
 def test_python_ver():
-    import sys
     req_ver = (2, 7)
     cur_ver = sys.version_info
     assert cur_ver >= req_ver
 
 
 def test_numpy():
-    numpy_meta = pkg_resources.get_distribution("numpy")
-    assert numpy_meta.version == '1.6.2'
-#    print(numpy_meta.parsed_version)
-#    assert numpy_meta.parsed_version >= (1, 5, 3)
+    numpy_meta = GD("numpy")
+    assert numpy_meta.parsed_version >= V('1.6.2')
 
 
 def test_traits():
-    traits_meta  = pkg_resources.get_distribution("traits")
-    assert traits_meta.version == '4.2.0'
+    traits_meta  = GD("traits")
+    assert traits_meta.parsed_version >= V('4.2.0')
 
 
 def test_pyparsing():
-    pyparsing_meta  = pkg_resources.get_distribution("pyparsing")
-    assert pyparsing_meta.version == '1.5.6'
+    pyparsing_meta  = GD("pyparsing")
+    # FIXME: SVG button stuff is failing with 2.0.1
+    assert pyparsing_meta.parsed_version == V('1.5.6')
 
 
 def test_xlrd():
-    xlrd_meta = pkg_resources.get_distribution("xlrd")
-    assert xlrd_meta.version == '0.8.0'
+    xlrd_meta = GD("xlrd")
+    assert xlrd_meta.parsed_version >= V('0.8.0')
 
 
 def test_openpyxl():
-    openpyxl_meta = pkg_resources.get_distribution("openpyxl")
-    assert openpyxl_meta.version == '1.5.8'
+    openpyxl_meta = GD("openpyxl")
+    assert openpyxl_meta.parsed_version >= V('1.5.8')
 
 
 def test_pandas():
-    pandas_meta = pkg_resources.get_distribution("pandas")
-    assert pandas_meta.version == '0.10.0'
+    pandas_meta = GD("pandas")
+    assert pandas_meta.parsed_version >= V('0.10.0')
 
 
 def test_pyper():
-    pyper_meta = pkg_resources.get_distribution("pyper")
-    assert pyper_meta.version == '1.1.1'
+    pyper_meta = GD("pyper")
+    assert pyper_meta.parsed_version >= V('1.1.1')
+
+
+def test_colormath():
+    color_meta = GD("colormath")
+    assert color_meta.parsed_version == V('1.0.8')
 
 
 def test_wxpython():
-    ## wx_meta = pkg_resources.get_distribution("wx")
-    ## assert wx_meta.version == '1.1.1'
+    ## wx_meta = GD("wx")
     import wx
-    wx_ver = wx.__version__
-    assert wx_ver == '2.8.12.1'
+    wx_ver = V(wx.__version__)
+    assert wx_ver >= V('2.8.12.1')
     del(wx)
 
 
 def test_pyside():
-    pyside_meta = pkg_resources.get_distribution("PySide")
-    assert pyside_meta.version == '1.1.2'
-
-
-def test_colormath():
-    color_meta = pkg_resources.get_distribution("colormath")
-    assert color_meta.version == '1.0.8'
+    # pyside_meta = GD("PySide")
+    import PySide
+    qt_ver = V(PySide.__version__)
+    assert qt_ver >= V('1.1.2')
+    del(PySide)
