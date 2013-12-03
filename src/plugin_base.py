d47c17ae9c3ebee0b26fc2d66d45f9e73253f84e
Thomas Graff <graff.thomas@gmail.com>
2013-12-03 05:39:46 +0100

    Change automatic plot window selection

--- src/plugin_base.py
+++ src/plugin_base.py
@@ -10,10 +10,10 @@ import chaco.api as _chaco
 # Local imports
 from dataset import DataSet
 from ds_table_view import DSTableViewer
-from plot_windows import PCPlotWindow
+from plot_pc_scatter import PCScatterPlot
+from plot_windows import PCPlotWindow, SinglePlotWindow
 from dataset_container import DatasetContainer
 from plugin_tree_helper import WindowLauncher
-
 
 
 class Result(_traits.HasTraits):
@@ -27,7 +27,9 @@ class Model(_traits.HasTraits):
     '''Base class for statistical analysis model'''
     id = _traits.Str()
     res = _traits.Property()
-    '''res is an object with the interesting results of one model calculation'''
+    '''res is an object with the interesting results
+    of one model calculation
+    '''
 
 
 class ModelController(_traitsui.Controller):
@@ -37,34 +39,28 @@ class ModelController(_traitsui.Controller):
     plot_uis = _traits.List()
     win_handle = _traits.Any()
 
-
     def init(self, info):
         super(ModelController, self).init(info)
         self.win_handle = info.ui.control
 
-
     def _name_default(self):
         raise NotImplementedError('_name_default')
-
 
     def __eq__(self, other):
         return self.id == other
 
-
     def __ne__(self, other):
         return self.id != other
 
-
     def get_result(self):
         return self.model.res
-
 
     def open_window(self, viewable, view_loop):
         """Expected viewable is by now:
           + Plot subtype
           + DataSet type
         """
-        if isinstance(viewable, _chaco.DataView):
+        if isinstance(viewable, PCScatterPlot):
             res = self.get_result()
 
             win = PCPlotWindow(
@@ -72,16 +68,29 @@ class ModelController(_traitsui.Controller):
                 res=res,
                 view_loop=view_loop,
                 # title_text=self._wind_title(res),
-                vistog=False
+                # vistog=False
+                )
+
+            self._show_plot_window(win)
+        elif isinstance(viewable, _chaco.DataView):
+            res = self.get_result()
+
+            win = SinglePlotWindow(
+                plot=viewable,
+                res=res,
+                view_loop=view_loop,
+                # title_text=self._wind_title(res),
+                # vistog=False
                 )
 
             self._show_plot_window(win)
         elif isinstance(viewable, DataSet):
             table = DSTableViewer(viewable)
-            table.edit_traits(view=table.get_view(), kind='live', parent=self.win_handle)
+            table.edit_traits(view=table.get_view(),
+                              kind='live',
+                              parent=self.win_handle)
         else:
             raise NotImplementedError("Do not know how to open this")
-
 
     def _show_plot_window(self, plot_window):
         # FIXME: Setting parent forcing main ui to stay behind plot windows
@@ -101,29 +110,24 @@ class ModelController(_traitsui.Controller):
             raise NotImplementedError(
                 "Not implemented for this platform: {0}".format(sys.platform))
 
-
     def _wind_title(self, res):
         ds_name = self.model.ds.display_name
         mn = res.method_name
         return "{0} | {1} - ConsumerCheck".format(ds_name, mn)
 
 
-
 class CalcContainer(_traits.HasTraits):
-    '''
+    '''The end
 
     '''
     dsc = _traits.Instance(DatasetContainer)
     calculations = _traits.List(_traits.HasTraits)
 
-
     def add(self, calc):
         self.calculations.append(calc)
 
-
     def remove(self, calc_id):
         del(self.calculations[self.calculations.index(calc_id)])
-
 
 
 class PluginController(_traitsui.Controller):
@@ -132,11 +136,9 @@ class PluginController(_traitsui.Controller):
     edit_node = _traits.Instance(ModelController)
     win_handle = _traits.Any()
 
-
     def init(self, info):
         super(PluginController, self).init(info)
         self.selected_object = self.model
-
 
     @_traits.on_trait_change('selected_object')
     def _tree_selection_made(self, obj, name, new):
@@ -151,7 +153,7 @@ class PluginController(_traitsui.Controller):
 # plugin_view
 def make_plugin_view(model_name, model_nodes, selection_view, model_view):
     node_label = '=' + model_name
-    container_nodes=[
+    container_nodes = [
         _traitsui.TreeNode(
             node_for=[CalcContainer],
             label=node_label,
@@ -178,12 +180,15 @@ def make_plugin_view(model_name, model_nodes, selection_view, model_view):
 
     plugin_view = _traitsui.View(
         _traitsui.Group(
-            _traitsui.Item('controller.model', editor=plugin_tree, show_label=False),
+            _traitsui.Item('controller.model',
+                           editor=plugin_tree,
+                           show_label=False),
             _traitsui.Group(
                 selection_view,
                 _traitsui.Group(
                     _traitsui.Item('controller.edit_node',
-                                   editor=_traitsui.InstanceEditor(view=model_view),
+                                   editor=_traitsui.InstanceEditor(
+                                       view=model_view),
                                    style='custom',
                                    show_label=False),
                     show_border=True,
@@ -198,8 +203,6 @@ def make_plugin_view(model_name, model_nodes, selection_view, model_view):
         )
 
     return plugin_view
-
-
 
 
 class TestOneNode(_traits.HasTraits):
