afa2d7a3d4c36b7ab24e58c56b22cb43203a9bdd
Thomas Graff <graff.thomas@gmail.com>
2015-01-21 23:36:06 +0100

    Release 1.1.0
    --HG--
    branch : release

--- make_wix.bat
+++ make_wix.bat
@@ -1,8 +1,8 @@
 rem *** Harvest files
-heat.exe dir consumercheck-1.0.5 -gg -srd -sreg -scom -cg CcFiles -dr CCROOTDIR -ke -generate components -out ccfiles.wxs
+heat.exe dir consumercheck-1.1.0 -gg -srd -sreg -scom -cg CcFiles -dr CCROOTDIR -ke -generate components -out ccfiles.wxs
 
 rem *** Run compiler
 candle ccfiles.wxs consumercheck.wxs
 
 rem *** Run linker
-light -b consumercheck-1.0.5 ccfiles.wixobj consumercheck.wixobj -ext WixUIExtension -out consumercheck-1.0.5.msi
+light -b consumercheck-1.1.0 ccfiles.wixobj consumercheck.wixobj -ext WixUIExtension -out consumercheck-1.1.0.msi
