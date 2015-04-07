a760c5bfc1b7eb92f0d9dfc728891c65c851c0f8
Thomas Graff <graff.thomas@gmail.com>
2015-04-07 16:53:44 +0200

    Update version requirements

--- ConsumerCheck/tests/runtime_env_test.py
+++ ConsumerCheck/tests/runtime_env_test.py
@@ -28,12 +28,12 @@ def test_python_ver():
 
 def test_numpy():
     numpy_meta = GD("numpy")
-    assert numpy_meta.parsed_version >= V('1.9.1')
+    assert numpy_meta.parsed_version >= V('1.9.0')
 
 
 def test_traits():
     traits_meta  = GD("traits")
-    assert traits_meta.parsed_version >= V('4.5.0')
+    assert traits_meta.parsed_version >= V('4.4.0')
 
 
 def test_pyparsing():
@@ -53,12 +53,13 @@ def test_openpyxl():
 
 def test_pandas():
     pandas_meta = GD("pandas")
-    assert pandas_meta.parsed_version >= V('0.15.0')
+    assert pandas_meta.parsed_version >= V('0.14.0')
 
 
 def test_pyper():
+    '''version 1.1.2: the row index is not importet to Pandas DataFrames'''
     pyper_meta = GD("pyper")
-    assert pyper_meta.parsed_version >= V('1.1.2')
+    assert pyper_meta.parsed_version >= V('1.1.1')
 
 
 def test_colormath():
@@ -66,24 +67,35 @@ def test_colormath():
     assert color_meta.parsed_version == V('2.1.0')
 
 
-def test_wxpython():
-    # wx_meta = GD("wx")
-    import wx
-    wx_ver = V(wx.__version__)
-    assert wx_ver >= V('2.8.12.1')
-    del(wx)
+#def test_wxpython():
+#    # wx_meta = GD("wx")
+#    import wx
+#    wx_ver = V(wx.__version__)
+#    assert wx_ver >= V('2.8.12.1')
+#    del(wx)
 
 
-@pytest.mark.xfail
-def test_pyside():
-    # pyside_meta = GD("PySide")
-    import PySide
-    qt_ver = V(PySide.__version__)
-    assert qt_ver >= V('1.1.2')
-    del(PySide)
+# @pytest.mark.xfail
+#def test_pyside():
+#    # pyside_meta = GD("PySide")
+#    import PySide
+#    qt_ver = V(PySide.__version__)
+#    assert qt_ver >= V('1.1.2')
+#    del(PySide)
 
 
-@pytest.mark.skipif(sys.platform != 'win32', reason='Only on win dev')
-def test_bbfreeze():
-    freeze_meta = GD("bbfreeze")
-    assert freeze_meta.parsed_version == V('1.1.3')
+# @pytest.mark.xfail
+def test_pyqt4():
+    from PyQt4.QtCore import QT_VERSION_STR
+    from PyQt4.Qt import PYQT_VERSION_STR
+    from sip import SIP_VERSION_STR
+    print("Qt version:", QT_VERSION_STR)
+    print("SIP version:", SIP_VERSION_STR)
+    print("PyQt version:", PYQT_VERSION_STR)
+    assert V(PYQT_VERSION_STR) >= V('4.10.0')
+
+
+#@pytest.mark.skipif(sys.platform != 'win32', reason='Only on win dev')
+#def test_bbfreeze():
+#    freeze_meta = GD("bbfreeze")
+#    assert freeze_meta.parsed_version == V('1.1.3')
