2c68d0dc4ecd656e512b7938d87bbfd437714e5a
Thomas Graff <graff.thomas@gmail.com>
2016-01-29 23:20:09 +0100

    Update version to 1.4.0

--- pyi-wix.bat
+++ pyi-wix.bat
@@ -1,8 +1,8 @@
 rem *** Harvest files
-heat.exe dir pyi-build-cc-1.3.3\ConsumerCheck -gg -srd -sreg -scom -cg CcFiles -dr CCROOTDIR -ke -generate components -out pyi-cc-files.wxs
+heat.exe dir pyi-dist-1.4.0\ConsumerCheckWin32 -gg -srd -sreg -scom -cg CcFiles -dr CCROOTDIR -ke -generate components -out pyi-cc-files.wxs
 
 rem *** Run compiler
 candle pyi-cc-files.wxs pyi-wix-cc-instinfo.wxs
 
 rem *** Run linker
-light -b pyi-build-cc-1.3.3\ConsumerCheck pyi-cc-files.wixobj pyi-wix-cc-instinfo.wixobj -ext WixUIExtension -out pyi-cc-install-1.3.3.msi
+light -b pyi-dist-1.4.0\ConsumerCheckWin32 pyi-cc-files.wixobj pyi-wix-cc-instinfo.wixobj -ext WixUIExtension -out cc-install-1.4.0.msi
