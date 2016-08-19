16703eb2b730840a6c3b5099d47b5c97ce40bdf4
Thomas Graff <graff.thomas@gmail.com>
2016-08-19 10:11:48 +0200

    Add OS X pyinstaller hooks

--- pyi-hooks/hook-lib2to3.py
+++ pyi-hooks/hook-lib2to3.py
@@ -1,3 +1,11 @@
-datas = [
-    (r'C:\Users\thomas\Miniconda2\envs\cc-env\Lib\lib2to3\Grammar.txt', 'lib2to3'),
-    (r'C:\Users\thomas\Miniconda2\envs\cc-env\Lib\lib2to3\PatternGrammar.txt', 'lib2to3')]
+import sys
+
+if sys.platform == 'darwin':
+	datas = [
+		(r'/Users/thomas/miniconda/envs/cc-env/lib/python2.7/lib2to3/Grammar.txt', 'lib2to3'),
+		(r'/Users/thomas/miniconda/envs/cc-env/lib/python2.7/lib2to3/PatternGrammar.txt', 'lib2to3')]
+else:
+	datas = [
+		(r'C:\Users\thomas\Miniconda2\envs\cc-env\Lib\lib2to3\Grammar.txt', 'lib2to3'),
+		(r'C:\Users\thomas\Miniconda2\envs\cc-env\Lib\lib2to3\PatternGrammar.txt', 'lib2to3')]
+
