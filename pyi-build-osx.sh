895b4a1e4ab6558dfd5579ca502946a06a3d3b4f
Thomas Graff <graff.thomas@gmail.com>
2016-09-28 22:30:20 +0200

    Update OSX build script

--- pyi-build-osx.sh
+++ pyi-build-osx.sh
@@ -1,11 +1,30 @@
 #! /bin/sh
-# --onefile --windowed
-# pyinstaller --log-level=INFO --clean --noconfirm --workpath=pyi-workdir --distpath=pyi-dist-1.4.0 pyi-cc-osx.spec
+# --debug --onedir --onefile --windowed --console --icon --specpath
+#
 pyinstaller --noconfirm --log-level=INFO \
-	--clean --onefile --windowed \
-	--path=ConsumerCheck \
-	--workpath=pyi-workdir \
-	--distpath=pyi-dist-1.4.0 \
-	--additional-hooks-dir=pyi-hooks \
-	--runtime-hook=pyi-rthook_pyqt4.py \
-	ConsumerCheck/cc_start.py
+	    --clean \
+	    --workpath="pyi-workdir" \
+	    --distpath="pyi-dist-1.4.1" \
+	    pyi-specs/Consumercheck.spec
+
+# From version 3.3.x
+#	    --add-data="ConsumerCheck/*.png:." \
+#	    --add-data="ConsumerCheck/*.svg:." \
+#	    --add-data="ConsumerCheck/graphics:graphics" \
+#	    --add-data="docs-user/build/html:help-docs" \
+
+# pyi-makespec
+# pyinstaller --noconfirm --log-level=INFO \
+# 	    --clean --onefile --windowed \
+# 	    --specpath="pyi-specs" \
+# 	    --name="Consumercheck" \
+# 	    --path="ConsumerCheck" \
+# 	    --additional-hooks-dir="pyi-hooks" \
+# 	    --runtime-hook=pyi-rthook_pyqt4.py \
+#           --icon="osx-cc.icns" \
+# 	    --osx-bundle-identifier="no.tgxnet.nofima.consumercheck" \
+# 	    ConsumerCheck/cc_start.py
+
+# pkgbuild --install-location /Applications --root pyi-specs/pyi-dist-1.4.0 cc-osx-1.4.0.pkg
+# pkgbuild --analyze --root pyi-specs/pyi-dist-1.4.0 cc-osx-1.4.0.plist
+hdiutil create ConsumerCheck_1.4.1_MacOSX.dmg -volname "CC dist" -srcfolder pyi-specs/pyi-dist-1.4.1
---
+++ pyi-specs/Consumercheck.spec
@@ -0,0 +1,46 @@
+# -*- mode: python -*-
+
+block_cipher = None
+
+added_files = [
+    ('../ConsumerCheck/*.png', '.'),
+    ('../ConsumerCheck/*.svg', '.'),
+    ('../ConsumerCheck/graphics', 'graphics'),
+    ('../docs-user/build/html', 'help-docs'),
+    ]
+
+a = Analysis(['../ConsumerCheck/cc_start.py'],
+             pathex=['ConsumerCheck', 'pyi-specs'],
+             binaries=None,
+             datas=added_files,
+             hiddenimports=[],
+             hookspath=['pyi-hooks'],
+             runtime_hooks=['pyi-rthook_pyqt4.py'],
+             excludes=[],
+             win_no_prefer_redirects=False,
+             win_private_assemblies=False,
+             cipher=block_cipher)
+
+pyz = PYZ(a.pure, a.zipped_data,
+             cipher=block_cipher)
+
+exe = EXE(pyz,
+          a.scripts,
+          a.binaries,
+          a.zipfiles,
+          a.datas,
+          name='Consumercheck',
+          debug=False,
+          strip=False,
+          upx=True,
+          console=False )
+
+app = BUNDLE(exe,
+             name='Consumercheck.app',
+             icon='osx-cc.icns',
+             bundle_identifier='no.tgxnet.nofima.consumercheck',
+             info_plist={
+                 'NSHighResolutionCapable': 'True',
+                 'CFBundleVersion': '1.4.1',
+                 },
+             )
