ab7a0ce6c35875e351cd74ce5ad5432c6ea92ffe
Thomas Graff <graff.thomas@gmail.com>
2016-09-28 21:01:56 +0200

    Add window icon

--- pyi-build-win.bat
+++ pyi-build-win.bat
@@ -3,6 +3,7 @@ pyinstaller.exe ^
     --log-level=INFO ^
     --clean ^
     --noconfirm ^
+    --icon win-cc.ico ^
     --workpath=pyi-workdir ^
     --distpath=pyi-dist-1.4.0 ^
     pyi-cc-win.spec
