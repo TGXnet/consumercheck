00471d9ebcb6a5afcaccdcb3eb1d420c82e03ab1
Thomas Graff <graff.thomas@gmail.com>
2017-03-21 13:00:47 +0100

    Initial checkin dummify function

---
+++ ConsumerCheck/dummify.py
@@ -0,0 +1,11 @@
+
+
+def dummify(dataset, attributes):
+    """Return dataset where selected attributes is dummified
+
+    :param dataset: the dataset to dummify
+    :param attributes: list of attributes to dummify
+    :returns: dataset with dummify selected variables
+    :raises keyError: raises an exception
+    """
+    pass
