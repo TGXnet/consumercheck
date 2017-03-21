d1ba9b260d1595f89ff903930c770a9a14a8f56c
Thomas Graff <graff.thomas@gmail.com>
2017-03-21 13:01:34 +0100

    Add test code

--- ConsumerCheck/dummify.py
+++ ConsumerCheck/dummify.py
@@ -1,4 +1,12 @@
 
+from __future__ import absolute_import, division, print_function
+
+
+import pandas as pd
+
+import dataset as ds
+
+
 
 def dummify(dataset, attributes):
     """Return dataset where selected attributes is dummified
@@ -8,4 +16,19 @@ def dummify(dataset, attributes):
     :returns: dataset with dummify selected variables
     :raises keyError: raises an exception
     """
-    pass
+    return dataset
+
+
+if __name__ == '__main__':
+    print("Start")
+    tor = ds.DataSet(display_name='Some values', kind='Consumer characteristics')
+    tor.mat = pd.DataFrame(
+        [[1, 'M', 'Good'],
+         [2, 'F', 'Poor'],
+         [1, 'M', 'Alive'],
+         [2, 'F', 'Good']],
+        index = ['V1', 'V2', 'V3', 'V4'],
+        columns = ['AgeGr', 'Sex', 'Health'])
+
+    res = dummify(tor, [])
+    print(res.mat)
