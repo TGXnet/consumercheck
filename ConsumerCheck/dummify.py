7de0b52fa6b718c15ccb1a8d72b75f11a535647f
Thomas Graff <graff.thomas@gmail.com>
2017-03-22 14:25:45 +0100

    Working dummify

--- ConsumerCheck/dummify.py
+++ ConsumerCheck/dummify.py
@@ -2,25 +2,31 @@
 from __future__ import absolute_import, division, print_function
 
 
-import pandas as pd
-
-import dataset as ds
-
-
-
 def dummify(dataset, attributes):
     """Return dataset where selected attributes is dummified
 
     :param dataset: the dataset to dummify
     :param attributes: list of attributes to dummify
     :returns: dataset with dummify selected variables
-    :raises keyError: raises an exception
+    :raises keyError: attribute not found in given dataset
     """
+    mat = dataset.mat
+    for attr in attributes:
+        pos = list(dataset.mat.columns).index(attr)
+        for val in set(mat[attr]):
+            attn = str("{}_{}".format(attr, val))
+            dummies = [int(i) for i in list(mat[attr] == val)]
+            pos += 1
+            mat.insert(pos, attn, dummies)
+        del(mat[attr])
     return dataset
 
 
 if __name__ == '__main__':
     print("Start")
+    import pandas as pd
+    import dataset as ds
+
     tor = ds.DataSet(display_name='Some values', kind='Consumer characteristics')
     tor.mat = pd.DataFrame(
         [[1, 'M', 'Good'],
@@ -30,5 +36,6 @@ if __name__ == '__main__':
         index = ['V1', 'V2', 'V3', 'V4'],
         columns = ['AgeGr', 'Sex', 'Health'])
 
-    res = dummify(tor, [])
+    print(tor.mat)
+    res = dummify(tor, ['AgeGr', 'Sex', 'Health'])
     print(res.mat)
