792889ed1e9a7eff04a04e1e729c1dd499428817
Thomas Graff <graff.thomas@gmail.com>
2017-08-23 12:21:30 +0200

    Extracted individual differences testing code

--- ConsumerCheck/ind_diff_gui.py
+++ ConsumerCheck/ind_diff_gui.py
@@ -482,39 +482,3 @@ selection_view = _traitsui.Group(
 
 ind_diff_plugin_view = pb.make_plugin_view(
     'IndDiff', ind_diff_nodes, selection_view, ind_diff_view)
-
-
-if __name__ == '__main__':
-    print("IndDiff GUI test start")
-    from tests.conftest import imp_ds
-    one_branch = False
-
-    # Folder, File name, Display name, DS type
-    cheese_L_meta = ('Cheese', 'ConsumerLiking.txt',
-                     'Cheese liking', 'Consumer liking')
-    cheese_A_meta = ('Cheese', 'ConsumerValues.txt',
-                     'Consumer values', 'Consumer characteristics')
-    ham_L_meta = ('HamData', 'Ham_consumer_liking_categories.csv',
-                  'Ham linking - categories', 'Consumer liking')
-    ham_A_meta = ('HamData', 'Ham_consumer_attributes_categories.csv',
-                  'Ham attributes - categories', 'Consumer characteristics')
-    CL = imp_ds(*cheese_L_meta)
-    CA = imp_ds(*cheese_A_meta)
-    HL = imp_ds(*ham_L_meta)
-    HA = imp_ds(*ham_A_meta)
-
-    if one_branch:
-        ind_diff = idm.IndDiff(ds_L=CL, ds_A=CA)
-        pc = IndDiffController(ind_diff)
-        test = pb.TestOneNode(one_model=pc)
-        test.configure_traits(view=pb.dummy_view(ind_diff_nodes))
-    else:
-        dsc = dc.DatasetContainer()
-        dsc.add(imp_ds(*ham_L_meta))
-        dsc.add(imp_ds(*ham_A_meta))
-        dsc.add(imp_ds(*cheese_L_meta))
-        dsc.add(imp_ds(*cheese_A_meta))
-        ind_diff = IndDiffCalcContainer(dsc=dsc)
-        ppc = IndDiffPluginController(ind_diff)
-        ppc.configure_traits(
-            view=ind_diff_plugin_view)
