545901f5d979f89ad624b18937d0d3ac5c0dea82
Thomas Graff <graff.thomas@gmail.com>
2018-01-01 20:35:34 +0100

    Update and cleanup Pyi Win build

--- pyi-build-win.bat
+++ pyi-build-win.bat
@@ -1,9 +1,25 @@
 @echo on
+REM	--specpath=dist ^
+REM	--hiddenimport=hoggorm ^
+REM	--add-data="docs-user/build/html;help-docs" ^
+REM 	--debug ^
 pyinstaller.exe ^
-    --log-level=WARN ^
-    --clean ^
-    --noconfirm ^
-    --icon win-cc.ico ^
-    --workpath=pyi-workdir ^
-    --distpath=pyi-dist-1.5.1 ^
-    pyi-cc-win.spec
+	--log-level=WARN ^
+	--clean ^
+	--noconfirm ^
+	--onedir ^
+	--console ^
+	--icon=win-cc.ico ^
+	--name=ccwin ^
+	--add-data="ConsumerCheck/*.png;." ^
+	--add-data="ConsumerCheck/*.svg;." ^
+	--add-data="ConsumerCheck/graphics;." ^
+	--add-data="ConsumerCheck/VERSION.txt;." ^
+	--add-data="ConsumerCheck/about.tmpl;." ^
+	--add-data="Rdist/R-3.3.1;R-3.3.1" ^
+	--additional-hooks-dir=pyi-hooks ^
+	--runtime-hook=pyi-rthook_pyqt4.py ^
+	--workpath=build ^
+	--distpath=dist ^
+	--path=ConsumerCheck ^
+	ConsumerCheck\cc_start.py
