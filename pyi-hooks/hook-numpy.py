209ffd248cfc3b01bd0175894a7a8cfb1aff37c5
Thomas Graff <graff.thomas@gmail.com>
2018-01-03 22:46:00 +0100

    Disable numpy MKL packaging for OSX

--- pyi-hooks/hook-numpy.py
+++ pyi-hooks/hook-numpy.py
@@ -2,11 +2,15 @@
 
 from PyInstaller import log as logging
 from PyInstaller import compat
+import sys
 from os import listdir
 from os.path import join
 
-logger = logging.getLogger(__name__)
-logger.info("MKL installed as part of numpy, importing that!")
+if sys.platform == 'darwin':
+    pass
+else:
+    logger = logging.getLogger(__name__)
+    logger.info("MKL installed as part of numpy, importing that!")
 
-mkldir = join(compat.base_prefix, "Library", "bin")
-binaries = [(join(mkldir, mkl), '') for mkl in listdir(mkldir) if mkl.startswith('mkl_')]
+    mkldir = join(compat.base_prefix, "Library", "bin")
+    binaries = [(join(mkldir, mkl), '') for mkl in listdir(mkldir) if mkl.startswith('mkl_')]
