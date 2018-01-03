5f9ad1bdad845de259d8db346a6d22042d949b13
Thomas Graff <graff.thomas@gmail.com>
2018-01-03 22:45:04 +0100

    Update OSX build script

--- pyi-build-osx.sh
+++ pyi-build-osx.sh
@@ -1,30 +1,44 @@
 #! /bin/sh
-# --debug --onedir --onefile --windowed --console --icon --specpath
-#
-pyinstaller --noconfirm --log-level=INFO \
-	    --clean \
-	    --workpath="pyi-workdir" \
-	    --distpath="pyi-dist-1.4.2" \
-	    pyi-specs/Consumercheck.spec
+# --log-level=TRACE, DEBUG, INFO, WARN, ERROR, CRITICAL
+# --debug --onedir --onefile --windowed --console
+# --osx-bundle-identifier='no.tgxnet.nofima.consumercheck' \
+# --icon='osx-cc.icns' \
+pyinstaller --noconfirm \
+	--log-level=INFO \
+	--onefile \
+	--windowed \
+	--name=ConsumerCheck \
+	--add-data="ConsumerCheck/*.png:." \
+	--add-data="ConsumerCheck/*.svg:." \
+	--add-data="ConsumerCheck/graphics:graphics" \
+	--add-data="ConsumerCheck/VERSION.txt:." \
+	--add-data="ConsumerCheck/about.tmpl:." \
+	--additional-hooks-dir=pyi-hooks \
+	--runtime-hook=pyi-rthook_pyqt4.py \
+	--paths="ConsumerCheck" \
+	--paths="pyi-specs" \
+	--icon='osx-cc.icns' \
+	--osx-bundle-identifier='no.tgxnet.nofima.consumercheck' \
+	ConsumerCheck/cc_start.py
 
-# From version 3.3.x
-#	    --add-data="ConsumerCheck/*.png:." \
-#	    --add-data="ConsumerCheck/*.svg:." \
-#	    --add-data="ConsumerCheck/graphics:graphics" \
-#	    --add-data="docs-user/build/html:help-docs" \
 
-# pyi-makespec
-# pyinstaller --noconfirm --log-level=INFO \
-# 	    --clean --onefile --windowed \
-# 	    --specpath="pyi-specs" \
-# 	    --name="Consumercheck" \
-# 	    --path="ConsumerCheck" \
-# 	    --additional-hooks-dir="pyi-hooks" \
-# 	    --runtime-hook=pyi-rthook_pyqt4.py \
-#           --icon="osx-cc.icns" \
-# 	    --osx-bundle-identifier="no.tgxnet.nofima.consumercheck" \
-# 	    ConsumerCheck/cc_start.py
+# productbuild?
+# xcodebuild?
+# COMPONENT PROPERTY LIST?
+# pkgutil
+# installer
+# --install-location
+# 
+# --root
+# --identifier no.tgxnet.nofima.consumercheck
+# --version 2.0.0
+# --analyze
 
-# pkgbuild --install-location /Applications --root pyi-specs/pyi-dist-1.4.0 cc-osx-1.4.0.pkg
+# FIXME: This is not working, Application is not to be found.
+# pkgbuild --install-location /Applications --identifier no.tgxnet.nofima.consumercheck --version 2.0.0 --root dist cc-osx-2.0.0.pkg
 # pkgbuild --analyze --root pyi-specs/pyi-dist-1.4.0 cc-osx-1.4.0.plist
-hdiutil create ConsumerCheck_1.4.2_MacOSX.dmg -volname "CC dist" -srcfolder pyi-specs/pyi-dist-1.4.2
+
+# This is working
+# rm dist/ConsumerCheck
+# -verbose
+# hdiutil create ConsumerCheck_2.0.0_MacOSX.dmg -volname "ConsumerCheck package" -srcfolder dist
