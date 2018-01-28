13049fdcf38f9739f0021b24dc523c6b2350c620
Thomas Graff <graff.thomas@gmail.com>
2018-01-28 13:26:45 +0100

    Clean Emacs config

--- .dir-locals.el
+++ .dir-locals.el
@@ -1,10 +1,6 @@
 ;;; Directory Local Variables
 ;;; For more information see (info "(emacs) Directory Variables")
 
-((nil
-  (ffip-project-file . ".hg")
-  (ffip-project-root . "."))
- (python-mode
+((python-mode
   (pyvenv-workon . cc-dev)
   (elpy-project-root . ".")))
-
